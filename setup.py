0d29170ceb696f1c0558f23425b07836f6612fb0
Tristan Van Berkom <tristan.vanberkom@codethink.co.uk>
2016-11-08 22:38:50 +0900

    Added plugins as package data.
    These are not part of the python package, but part
    of the data installed. Yes, it's a bit weird, but more
    correct than having the plugins as submodules of
    the actual buildstream python package.

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
-from setuptools import setup
-from setuptools import find_packages
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    print("BuildStream requires setuptools in order to build. Install it using"
+          " your package manager (usually python3-setuptools) or via pip (pip3"
+          " install setuptools).")
+    sys.exit(1)
 
 setup(name='buildstream',
       version='0.1',
       description='Framework for modelling of build pipelines in YAML',
       license='LGPL',
       packages=find_packages(),
+      package_data={'buildstream': ['plugins/*/*.py']},
       scripts=['bin/build-stream'],
       install_requires=[
 
