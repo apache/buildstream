2016ca6bd3b06ded0c730142c6c085cbf7e8f04b
Tristan Van Berkom <tristan.vanberkom@codethink.co.uk>
2016-11-13 21:21:54 +0900

    Adding test ensuring failure mode when plugin reports something not a type
    In the case a plugin defines a setup() which reports a value instead
    of a type, we expect a PluginError.

--- tests/plugins/basics.py
+++ tests/plugins/basics.py
@@ -90,3 +90,17 @@ def test_conflict_element(plugin_fixture, datafiles):
 
     with pytest.raises(PluginError) as exc:
         factory = _ElementFactory(plugin_fixture['base'], [ plugins1, plugins2 ])
+
+# Load a factory with a plugin that returns a value instead of a Source subclass
+@pytest.mark.datafiles(os.path.join(DATA_DIR, 'notatype'))
+def test_source_notatype(plugin_fixture, datafiles):
+    with pytest.raises(PluginError) as exc:
+        factory = _SourceFactory(plugin_fixture['base'],
+                                  [ os.path.join(datafiles.dirname, datafiles.basename) ])
+
+# Load a factory with a plugin that returns a value instead of a Element subclass
+@pytest.mark.datafiles(os.path.join(DATA_DIR, 'notatype'))
+def test_element_notatype(plugin_fixture, datafiles):
+    with pytest.raises(PluginError) as exc:
+        factory = _ElementFactory(plugin_fixture['base'],
+                                  [ os.path.join(datafiles.dirname, datafiles.basename) ])
---
+++ tests/plugins/basics/notatype/foo.py
@@ -0,0 +1,4 @@
+# Plugins are supposed to return a subclass type
+# of Source or Element, depending on plugin type.
+def setup():
+    return 5
