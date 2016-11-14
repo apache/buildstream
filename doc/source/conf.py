cfd685f85e3569c4aea67aa5f278a73464af40f4
Tristan Van Berkom <tristan.vanberkom@codethink.co.uk>
2016-11-14 17:19:03 +0900

    documentation config changes
      * Dont show module paths on function/class docs
      * Sort the module index ignoring the buildstream prefix

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -98,7 +98,7 @@ exclude_patterns = []
 # If true, the current module name will be prepended to all description
 # unit titles (such as .. function::).
 #
-# add_module_names = True
+add_module_names = False
 
 # If true, sectionauthor and moduleauthor directives will be shown in the
 # output. They are ignored by default.
@@ -109,7 +109,7 @@ exclude_patterns = []
 pygments_style = 'sphinx'
 
 # A list of ignored prefixes for module index sorting.
-# modindex_common_prefix = []
+modindex_common_prefix = [ 'buildstream.' ]
 
 # If true, keep warnings as "system message" paragraphs in the built documents.
 # keep_warnings = False
