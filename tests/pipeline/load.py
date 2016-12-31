05a8175576574df8666d6603347731850418f225
Tristan Van Berkom <tristan.vanberkom@codethink.co.uk>
2016-12-31 15:26:03 -0500

    Added tests with pipeline to check element iteration and scope

--- tests/pipeline/load.py
+++ tests/pipeline/load.py
@@ -1,7 +1,7 @@
 import os
 import pytest
 
-from buildstream import Context, Project
+from buildstream import Context, Project, Scope
 from buildstream._pipeline import Pipeline
 
 DATA_DIR = os.path.join(
@@ -32,3 +32,106 @@ def test_load_simple(datafiles, tmpdir):
     command_list = pipeline.target.commands['configure-commands']
     assert(len(command_list) == 1)
     assert(command_list[0] == 'pony')
+
+
+###############################################################
+#        Testing Element.dependencies() iteration             #
+###############################################################
+@pytest.mark.datafiles(os.path.join(DATA_DIR, 'iterate'))
+def test_iterate_scope_all(datafiles, tmpdir):
+
+    basedir = os.path.join(datafiles.dirname, datafiles.basename)
+    pipeline = create_pipeline(tmpdir, basedir, 'target.bst', None)
+
+    assert(pipeline.target.get_kind() == "autotools")
+
+    assert(isinstance(pipeline.target.commands['configure-commands'], list))
+    command_list = pipeline.target.commands['configure-commands']
+    assert(len(command_list) == 1)
+    assert(command_list[0] == 'pony')
+
+    element_list = pipeline.target.dependencies(Scope.ALL)
+    element_list = list(element_list)
+    assert(len(element_list) == 7)
+
+    assert(element_list[0].name == "build-build")
+    assert(element_list[1].name == "run-build")
+    assert(element_list[2].name == "build")
+    assert(element_list[3].name == "dep-one")
+    assert(element_list[4].name == "run")
+    assert(element_list[5].name == "dep-two")
+    assert(element_list[6].name == "target")
+
+
+@pytest.mark.datafiles(os.path.join(DATA_DIR, 'iterate'))
+def test_iterate_scope_run(datafiles, tmpdir):
+
+    basedir = os.path.join(datafiles.dirname, datafiles.basename)
+    pipeline = create_pipeline(tmpdir, basedir, 'target.bst', None)
+
+    assert(pipeline.target.get_kind() == "autotools")
+
+    assert(isinstance(pipeline.target.commands['configure-commands'], list))
+    command_list = pipeline.target.commands['configure-commands']
+    assert(len(command_list) == 1)
+    assert(command_list[0] == 'pony')
+
+    element_list = pipeline.target.dependencies(Scope.RUN)
+    element_list = list(element_list)
+    assert(len(element_list) == 4)
+
+    assert(element_list[0].name == "dep-one")
+    assert(element_list[1].name == "run")
+    assert(element_list[2].name == "dep-two")
+    assert(element_list[3].name == "target")
+
+
+@pytest.mark.datafiles(os.path.join(DATA_DIR, 'iterate'))
+def test_iterate_scope_build(datafiles, tmpdir):
+
+    basedir = os.path.join(datafiles.dirname, datafiles.basename)
+    pipeline = create_pipeline(tmpdir, basedir, 'target.bst', None)
+
+    assert(pipeline.target.get_kind() == "autotools")
+
+    assert(isinstance(pipeline.target.commands['configure-commands'], list))
+    command_list = pipeline.target.commands['configure-commands']
+    assert(len(command_list) == 1)
+    assert(command_list[0] == 'pony')
+
+    element_list = pipeline.target.dependencies(Scope.BUILD)
+    element_list = list(element_list)
+
+    assert(len(element_list) == 3)
+
+    assert(element_list[0].name == "dep-one")
+    assert(element_list[1].name == "run")
+    assert(element_list[2].name == "dep-two")
+
+
+@pytest.mark.datafiles(os.path.join(DATA_DIR, 'iterate'))
+def test_iterate_scope_build_of_child(datafiles, tmpdir):
+
+    basedir = os.path.join(datafiles.dirname, datafiles.basename)
+    pipeline = create_pipeline(tmpdir, basedir, 'target.bst', None)
+
+    assert(pipeline.target.get_kind() == "autotools")
+
+    assert(isinstance(pipeline.target.commands['configure-commands'], list))
+    command_list = pipeline.target.commands['configure-commands']
+    assert(len(command_list) == 1)
+    assert(command_list[0] == 'pony')
+
+    # First pass, lets check dep-two
+    element_list = pipeline.target.dependencies(Scope.BUILD)
+    element_list = list(element_list)
+    element = element_list[2]
+
+    # Pass two, let's look at these
+    element_list = element.dependencies(Scope.BUILD)
+    element_list = list(element_list)
+
+    assert(len(element_list) == 2)
+
+    assert(element_list[0].name == "run-build")
+    assert(element_list[1].name == "build")
