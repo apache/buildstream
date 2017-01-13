8c58f06a1f1007c14f043590479974c5d72c5419
Tristan Van Berkom <tristan.vanberkom@codethink.co.uk>
2017-01-13 16:23:26 -0500

    _message.py: Added message depth and INFO message types

--- buildstream/_message.py
+++ buildstream/_message.py
@@ -25,11 +25,12 @@ import os
 #
 class MessageType():
     DEBUG = "debug"        # Debugging message
-    STATUS = "status"      # Status message
+    STATUS = "status"      # Status message, verbose details
+    INFO = "info"          # Informative messages
     WARN = "warning"       # Warning messages
     ERROR = "error"        # Error messages
 
-    # The following types are timed, SUCCESS/FAIL have timestamps
+    # Timed Messages: SUCCESS and FAIL have duration timestamps
     START = "start"        # Status start message
     SUCCESS = "success"    # Successful status complete message
     FAIL = "failure"       # Failing status complete message
@@ -38,17 +39,20 @@ class MessageType():
 # Message object
 #
 class Message():
+
     def __init__(self, unique_id, message_type, message,
                  detail=None,
                  action_name=None,
                  elapsed=None,
+                 depth=None,
                  logfile=None,
                  scheduler=False):
         self.message_type = message_type  # Message type
         self.message = message            # The message string
         self.detail = detail              # An additional detail string
         self.action_name = action_name    # Name of the task queue (fetch, refresh, build, etc)
-        self.elapsed = elapsed            # The elapsed time, for SUCCESS and FAIL types
+        self.elapsed = elapsed            # The elapsed time, in timed messages
+        self.depth = depth                # The depth of a timed message
         self.logfile = logfile            # The log file path where commands took place
         self.pid = os.getpid()            # The process pid
         self.unique_id = unique_id        # The plugin object unique identifier
