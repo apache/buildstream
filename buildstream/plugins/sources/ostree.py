f4a213859bd4ba06ac233a37f9135990a83c9116
Tristan Van Berkom <tristan.vanberkom@codethink.co.uk>
2017-01-16 15:14:54 -0500

    ostree.py source plugin: Use _ostree routines
    Also implement some missing methods and add some status messages.

--- buildstream/plugins/sources/ostree.py
+++ buildstream/plugins/sources/ostree.py
@@ -20,12 +20,42 @@
 
 """A Source implementation for OSTree based directory artifacts
 
+**Usage:**
+
+.. code:: yaml
+
+   # Specify the ostree source kind
+   kind: ostree
+
+   # Optionally specify a relative staging directory
+   # directory: path/to/stage
+
+   # Specify the repository url, using an alias defined
+   # in your project configuration is recommended.
+   url: upstream:runtime
+
+   # Optionally specify a symbolic tracking branch or tag, this
+   # will be used to update the 'ref' when refreshing the pipeline.
+   track: runtime/x86_64/stable
+
+   # Specify the commit checksum, this must be specified in order
+   # to checkout sources and build, but can be automatically
+   # updated if the 'track' attribute was specified.
+   ref: d63cbb6fdc0bbdadc4a1b92284826a6d63a7ebcd
+
+   # For signed ostree repositories, specify a local project relative
+   # path to the public verifying GPG key for this remote.
+   gpg-key: keys/runtime.gpg
 """
 
-from gi.repository import OSTree, Gio, GLib
-from gi.repository.GLib import Variant, VariantDict
+import os
+import tempfile
+import shutil
 
-from buildstream import Source, LoadError
+from buildstream import Source, SourceError
+from buildstream import utils
+from buildstream import _ostree
+from buildstream._ostree import OSTreeError
 
 
 class OSTreeSource(Source):
@@ -33,172 +63,109 @@ class OSTreeSource(Source):
     def configure(self, node):
         project = self.get_project()
 
-        self.remote_name = "origin"
-        # self.url = project.translate_url(self.node_get_member(node, str, 'url'))
-        self.url = self.node_get_member(node, str, 'url')
-        self.ref = self.node_get_member(node, str, 'ref')
-        self.track = self.node_get_member(node, str, 'track', '')
+        self.original_url = self.node_get_member(node, str, 'url')
+        self.url = project.translate_url(self.original_url)
+        self.ref = self.node_get_member(node, str, 'ref', '') or None
+        self.track = self.node_get_member(node, str, 'track', '') or None
+        self.mirror = os.path.join(self.get_mirror_directory(),
+                                   utils.url_directory_name(self.url))
 
         # (optional) Not all repos are signed. But if they are, get the gpg key
-        try:
-            self.gpg_key = self.node_get_member(node, str, 'gpg_key', None)
-        except LoadError:
-            self.gpg_key = None
+        self.gpg_key = self.node_get_member(node, str, 'gpg-key', '') or None
+        self.gpg_key_path = None
+        if self.gpg_key is not None:
+            self.gpg_key_path = os.path.join(project.directory, self.gpg_key)
 
-        self.ostree_dir = "repo"    # Assume to be some tmp dir
+        # Our OSTree repo handle
+        self.repo = None
+
+        if not (self.ref or self.track):
+            raise SourceError("Must specify either 'ref' or 'track' parameters")
 
     def preflight(self):
         return
 
     def get_unique_key(self):
-        return [self.url, self.ref]
+        return [self.original_url, self.ref]
 
     def refresh(self, node):
-        self.load_ostree(self.ostree_dir)
-        self.fetch_ostree(self.remote_name, self.ref)
-
-        # TODO Only return true if things have been updated. Not sure
-        # how I'd do this with OSTree. Surely the ref used means nothing
-        # is different, unless this has not been pulled yet.
-        return True
-
-    def fetch(self):
-        # Pull the OSTree from the remote
-
-        self.init_ostree(self.ostree_dir, self.remote_name, self.url)
-        self.fetch_ostree(self.remote_name, self.ref)
-
-    def stage(self, directory):
-        # Checkout self.ref into the specified directory
-
-        # TODO catch exception thrown for wrong ref
-        self.checkout_ostree(directory, self.ref)
-
-    def consistent(self):
-        return True
-
-    ###########################################################
-    #                     Local Functions                     #
-    ###########################################################
-
-    def init_ostree(self, repo_dir, remote_name, remote_url):
-        # Initialises a new empty OSTree repo and set the mode
-        #
-        # cli example:
-        #   ostree --repo=repo init --mode=archive-z2
-
-        self.ost = OSTree.Repo.new(Gio.File.new_for_path(repo_dir))
-        self.ost.create(OSTree.RepoMode.ARCHIVE_Z2, None)
-        self.add_remote(remote_name, remote_url)
-
-    def load_ostree(self, repo_dir):
-        # Loads an existing OSTree repo from the given `repo_dir` and make sure we have an OSTree reference
-
-        self.ost = OSTree.Repo.new(Gio.File.new_for_path(repo_dir))
-        self.ost.open()
-
-    def fetch_ostree(self, remote, ref):
-        # Fetch metadata of the repo from a remote
-        #
-        # cli example:
-        #  ostree --repo=repo pull --mirror freedesktop:runtime/org.freedesktop.Sdk/x86_64/1.4
-
-        progress = None  # Alternatively OSTree.AsyncProgress, None assumed to block
-        cancellable = None  # Alternatively Gio.Cancellable
-
-        self.ost.pull(remote, None, OSTree.RepoPullFlags.MIRROR, progress, cancellable)
-
-    REMOTE_ADDED = 1
-    REMOTE_DUPLICATE = 2
-    REMOTE_KEY_FAIL = 3
-
-    def add_remote(self, name, url, key=None):
-        # Add a remote OSTree repo. If no key is given, we disable gpg checking.
-        # Returns REMOTE_* , where
-        #   REMOTE_ADDED is a success.
-        #   REMOTE_DUPLICATE if remote already exists
-        #   REMOTE_KEY_FAIL key could not be added for some reason (wrong file/ corrupt key)
-        #
-        # cli exmaple:
-        #   wget https://sdk.gnome.org/keys/gnome-sdk.gpg
-        #   ostree --repo=repo --gpg-import=gnome-sdk.gpg remote add freedesktop https://sdk.gnome.org/repo
-
-        options = None  # or GLib.Variant of type a{sv}
-        cancellable = None  # or Gio.Cancellable
-
-        if key is None:
-            vd = VariantDict.new()
-            vd.insert_value('gpg-verify', Variant.new_boolean(False))
-            options = vd.end()
-
-        try:
-            self.ost.remote_add(name, url, options, cancellable)
-        except GLib.GError:
-            return self.REMOTE_DUPLICATE
-
-        # Remote needs to exist before adding key
-        if key is not None:
-            try:
-                self.add_gpg_key(name, key)
-            except GLib.GError:
-                return self.REMOTE_KEY_FAIL
-
-        return self.REMOTE_ADDED
-
-    def add_gpg_key(self, remote, url):
-        # Add a gpg key for a given remote
-        #
-        # cli exmaple:
-        #   wget https://sdk.gnome.org/keys/gnome-sdk.gpg
-        #   ostree --repo=repo --gpg-import=gnome-sdk.gpg remote add freedesktop https://sdk.gnome.org/repo
-
-        gfile = Gio.File.new_for_uri(url)
-        stream = gfile.read()
-
-        self.ost.remote_gpg_import(remote, stream, None, 0, None)
-        return
-
-    def checkout_ostree(self, checkout_dir, ref):
-        # Check out a full copy of an OSTree at a given ref to some directory.
-        # Note: OSTree does not like updating directories inline/sync, therefore
-        # make sure you checkout to a clean directory or add additional code to support
-        # union mode or (if it exists) file replacement/update.
-        #
-        # Returns True on success
-        #
-        # cli exmaple:
-        #   ostree --repo=repo checkout --user-mode runtime/org.freedesktop.Sdk/x86_64/1.4 foo
-
-        options = OSTree.RepoCheckoutAtOptions()
-        # ignore uid/gid to allow checkout as non-root
-        options.mode = OSTree.RepoCheckoutMode.USER
-
-        # from fcntl.h
-        AT_FDCWD = -100
-        try:
-            self.ost.checkout_at(options, AT_FDCWD, checkout_dir, ref)
-            return True
-        except:
+        # If self.track is not specified it's not an error, just silently return
+        if not self.track:
             return False
 
-    def ls_tracks(self):
-        # Grab the named refs/tracks that exist in this repo
-        # ostree --repo=repo refs
-        _, refs = self.ost.list_refs()
+        self.ensure()
+        with self.timed_activity("Fetching tracking ref '{}' from origin: {}"
+                                 .format(self.track, self.url)):
+            try:
+                _ostree.fetch(self.repo, ref=self.track, progress=self.progress)
+            except OSTreeError as e:
+                raise SourceError("{}: Failed to fetch tracking ref '{}' from origin {}\n\n{}"
+                                  .format(self, self.track, self.url, e)) from e
 
-        # Returns a dict of {branch: head-ref}
-        return refs.keys()
+        new_ref = _ostree.checksum(self.repo, self.track)
+        if self.ref != new_ref:
+            node['ref'] = self.ref = new_ref
+            return True
 
-    def track_head(self, track_name):
-        # Get the checksum of the head commit of the branch `track_name`
-        # ostree --repo=repo log runtime/org.freedesktop.Sdk/x86_64/1.4
-        _, head_ref = self.ost.resolve_ref(track_name)
+        return False
 
-        return head_ref
+    def fetch(self):
+        self.ensure()
+        if not _ostree.exists(self.repo, self.ref):
+            with self.timed_activity("Fetching remote ref: {} from origin: {}"
+                                     .format(self.ref, self.url)):
+                try:
+                    _ostree.fetch(self.repo, ref=self.ref, progress=self.progress)
+                except OSTreeError as e:
+                    raise SourceError("{}: Failed to fetch ref '{}' from origin: {}\n\n{}"
+                                      .format(self, self.ref, self.url, e)) from e
 
-    def ls_files(self, ref):
-        # ostree --repo=repo ls -R 6fe05489235bcae562f0afa5aca9bb8d350bdf93ea8f4645adb694b907f48190
-        pass
+    def stage(self, directory):
+        self.ensure()
+
+        # Checkout self.ref into the specified directory
+        os.makedirs(self.get_mirror_directory(), exist_ok=True)
+        with tempfile.TemporaryDirectory(prefix='tmp', dir=self.get_mirror_directory()) as tmpdir:
+
+            checkoutdir = os.path.join(tmpdir, 'checkout')
+
+            with self.timed_activity("Staging ref: {} from origin: {}"
+                                     .format(self.ref, self.url)):
+                try:
+                    _ostree.checkout(self.repo, checkoutdir, self.ref)
+                except OSTreeError as e:
+                    raise SourceError("{}: Failed to checkout ref '{}' from origin: {}\n\n{}"
+                                      .format(self, self.ref, self.url, e)) from e
+
+            os.makedirs(os.path.dirname(directory), exist_ok=True)
+            try:
+                shutil.move(checkoutdir, directory)
+            except (shutil.Error, OSError) as e:
+                raise SourceError("{}: Failed to move ostree checkout {} from '{}' to '{}'\n\n{}"
+                                  .format(self, self.url, tmpdir, directory, e)) from e
+
+    def consistent(self):
+        return self.ref is not None
+
+    #
+    # Local helpers
+    #
+    def ensure(self):
+        if not self.repo:
+            self.status("Creating local mirror for {}".format(self.url))
+
+            self.repo = _ostree.ensure(self.mirror, True)
+            gpg_key = None
+            if self.gpg_key_path:
+                gpg_key = 'file://' + self.gpg_key_path
+
+            try:
+                _ostree.configure_origin(self.repo, self.url, key_url=gpg_key)
+            except OSTreeError as e:
+                raise SourceError("{}: Failed to configure origin {}\n\n{}".format(self, self.url, e)) from e
+
+    def progress(self, percent, message):
+        self.status(message)
 
 
 # Plugin entry point
