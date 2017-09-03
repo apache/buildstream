3027bc1e2b5711994427ec632eaff6ef7366b46a
Tristan Van Berkom <tristan.vanberkom@codethink.co.uk>
2017-09-03 18:47:32 -0400

    tests/frontend/compose_splits.py: Adding some simple artifact splitting test.
    This adds a couple of extra elements to the test data project.

---
+++ tests/frontend/compose_splits.py
@@ -0,0 +1,37 @@
+import os
+import pytest
+from tests.testutils.runcli import cli
+
+# Project directory
+DATA_DIR = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    "project",
+)
+
+
+@pytest.mark.parametrize("target", [
+    ('compose-include-bin.bst'),
+    ('compose-exclude-dev.bst')
+])
+@pytest.mark.datafiles(DATA_DIR)
+def test_compose_splits(datafiles, cli, target):
+    project = os.path.join(datafiles.dirname, datafiles.basename)
+    checkout = os.path.join(cli.directory, 'checkout')
+
+    # First build it
+    result = cli.run(project=project, args=['build', target])
+    assert result.exit_code == 0
+
+    # Now check it out
+    result = cli.run(project=project, args=[
+        'checkout', target, checkout
+    ])
+    assert result.exit_code == 0
+
+    # Check that the executable hello file is found in the checkout
+    filename = os.path.join(checkout, 'usr', 'bin', 'hello')
+    assert os.path.exists(filename)
+
+    # Check that the executable hello file is found in the checkout
+    filename = os.path.join(checkout, 'usr', 'include', 'pony.h')
+    assert not os.path.exists(filename)
