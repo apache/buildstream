99f7febbcf697ad198af4a493feb21f7f56529dc
Tristan Van Berkom <tristan.vanberkom@codethink.co.uk>
2017-09-04 11:56:15 -0400

    cachekey.py: Fix broken logic of excluding tests
    Skip the tests if we DONT have bzr or git, not if we DO.

--- tests/cachekey/cachekey.py
+++ tests/cachekey/cachekey.py
@@ -149,8 +149,8 @@ DATA_DIR = os.path.join(
 # The cache key test uses a project which exercises all plugins,
 # so we cant run it at all if we dont have them installed.
 #
-@pytest.mark.skipif(HAVE_BZR, reason="bzr is not available")
-@pytest.mark.skipif(HAVE_GIT, reason="git is not available")
+@pytest.mark.skipif(HAVE_BZR is False, reason="bzr is not available")
+@pytest.mark.skipif(HAVE_GIT is False, reason="git is not available")
 @pytest.mark.datafiles(DATA_DIR)
 def test_cache_key(datafiles, cli):
     project = os.path.join(datafiles.dirname, datafiles.basename)
