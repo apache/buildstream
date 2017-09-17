apache/buildstream

commit 6691d0eb7e20c5872d7a5f439f308ae46019777f

project.py: Add project config node validations

buildstream/project.py
@@ -158,6 +158,13 @@ class Project():
         # Load project local config and override the builtin
         project_conf = _yaml.load(projectfile)
         _yaml.composite(config, project_conf, typesafe=True)
+        _yaml.validate_node(config, [
+            'required-versions',
+            'element-path', 'variables',
+            'environment', 'environment-nocache',
+            'split-rules', 'elements', 'plugins',
+            'aliases', 'name'
+        ])
 
         # Resolve arches keyword, project may have arch conditionals
         _loader.resolve_arch(config, self._host_arch, self._target_arch)
@@ -233,6 +240,7 @@ class Project():
 
         # Version requirements
         versions = _yaml.node_get(self._unresolved_config, Mapping, 'required-versions')
+        _yaml.validate_node(versions, ['project', 'elements', 'sources'])
 
         # Assert project version first
         format_version = _yaml.node_get(versions, int, 'project')
@@ -259,6 +267,7 @@ class Project():
 
         # Load the plugin paths
         plugins = _yaml.node_get(self._unresolved_config, Mapping, 'plugins', default_value={})
+        _yaml.validate_node(plugins, ['elements', 'sources'])
         self._plugin_source_paths = [os.path.join(self.directory, path)
                                      for path in self._extract_plugin_paths(plugins, 'sources')]
         self._plugin_element_paths = [os.path.join(self.directory, path)

commit e62e71a6867cf5652d8c5afda43fba2c8df3dee3

project.py: Allow 'host-arches' and 'arches' in project.conf
This fixes a regression from 6691d0eb7e20c5872d7a5f439f308ae46019777f.

buildstream/project.py
@@ -163,7 +163,8 @@ class Project():
             'element-path', 'variables',
             'environment', 'environment-nocache',
             'split-rules', 'elements', 'plugins',
-            'aliases', 'name'
+            'aliases', 'name',
+            'arches', 'host-arches',
         ])
 
         # Resolve arches keyword, project may have arch conditionals