6819bb8c50126538ce8877b8f8c5fb98120001db
Tristan Van Berkom <tristan.vanberkom@codethink.co.uk>
2017-10-07 18:06:24 +0900

    tests/format/optioneltmask.py: Added element-mask option specific test cases

---
+++ tests/format/optioneltmask.py
@@ -0,0 +1,87 @@
+import os
+import pytest
+from buildstream import _yaml
+from buildstream import LoadError, LoadErrorReason
+from tests.testutils.runcli import cli
+
+# Project directory
+DATA_DIR = os.path.dirname(os.path.realpath(__file__))
+
+
+@pytest.mark.datafiles(DATA_DIR)
+@pytest.mark.parametrize("target,value,expected", [
+    ('pony.bst', 'pony.bst', 'True'),
+    ('horsy.bst', 'pony.bst, horsy.bst', 'True'),
+    ('zebry.bst', 'pony.bst, horsy.bst', 'False'),
+])
+def test_conditional_cli(cli, datafiles, target, value, expected):
+    project = os.path.join(datafiles.dirname, datafiles.basename, 'option-element-mask')
+    result = cli.run(project=project, silent=True, args=[
+        '--option', 'debug_elements', value,
+        'show',
+        '--deps', 'none',
+        '--format', '%{vars}',
+        target])
+
+    assert result.exit_code == 0
+    loaded = _yaml.load_data(result.output)
+    assert loaded['debug'] == expected
+
+
+@pytest.mark.datafiles(DATA_DIR)
+@pytest.mark.parametrize("target,value,expected", [
+    ('pony.bst', ['pony.bst'], 'True'),
+    ('horsy.bst', ['pony.bst', 'horsy.bst'], 'True'),
+    ('zebry.bst', ['pony.bst', 'horsy.bst'], 'False'),
+])
+def test_conditional_config(cli, datafiles, target, value, expected):
+    project = os.path.join(datafiles.dirname, datafiles.basename, 'option-element-mask')
+    cli.configure({
+        'projects': {
+            'test': {
+                'options': {
+                    'debug_elements': value
+                }
+            }
+        }
+    })
+    result = cli.run(project=project, silent=True, args=[
+        'show',
+        '--deps', 'none',
+        '--format', '%{vars}',
+        target])
+
+    assert result.exit_code == 0
+    loaded = _yaml.load_data(result.output)
+    assert loaded['debug'] == expected
+
+
+@pytest.mark.datafiles(DATA_DIR)
+def test_invalid_declaration(cli, datafiles):
+    project = os.path.join(datafiles.dirname, datafiles.basename, 'option-element-mask-invalid')
+    result = cli.run(project=project, silent=True, args=[
+        'show',
+        '--deps', 'none',
+        '--format', '%{vars}',
+        'pony.bst'])
+
+    assert result.exit_code != 0
+    assert result.exception
+    assert isinstance(result.exception, LoadError)
+    assert result.exception.reason == LoadErrorReason.INVALID_DATA
+
+
+@pytest.mark.datafiles(DATA_DIR)
+def test_invalid_value(cli, datafiles):
+    project = os.path.join(datafiles.dirname, datafiles.basename, 'option-element-mask')
+    result = cli.run(project=project, silent=True, args=[
+        '--option', 'debug_elements', 'kitten.bst',
+        'show',
+        '--deps', 'none',
+        '--format', '%{vars}',
+        'pony.bst'])
+
+    assert result.exit_code != 0
+    assert result.exception
+    assert isinstance(result.exception, LoadError)
+    assert result.exception.reason == LoadErrorReason.INVALID_DATA
