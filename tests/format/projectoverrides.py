b14a0c575991f3b75aa15b21b2652186b62d0a09
Tristan Van Berkom <tristan.vanberkom@codethink.co.uk>
2017-10-25 20:04:36 +0900

    test/format/projectoverrides.py: Test project wide element type overrides
    First case simply asserts that prepending to configure-commands
    from the project.conf works as intended, ensuring we dont regress
    issue #127

---
+++ tests/format/projectoverrides.py
@@ -0,0 +1,28 @@
+
+import os
+import pytest
+from buildstream import _yaml
+from buildstream import LoadError, LoadErrorReason
+from tests.testutils.runcli import cli
+
+# Project directory
+DATA_DIR = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    "project-overrides"
+)
+
+
+@pytest.mark.datafiles(DATA_DIR)
+def test_prepend_configure_commands(cli, datafiles):
+    project = os.path.join(datafiles.dirname, datafiles.basename, 'prepend-configure-commands')
+    result = cli.run(project=project, silent=True, args=[
+        'show',
+        '--deps', 'none',
+        '--format', '%{config}',
+        'element.bst'])
+
+    assert result.exit_code == 0
+    loaded = _yaml.load_data(result.output)
+    config_commands = loaded['configure-commands']
+    assert len(config_commands) == 3
+    assert config_commands[0] == 'echo "Hello World!"'
