139b7d98178100954c64cb788dbd4a01549f75ec
Tristan Van Berkom <tristan.vanberkom@codethink.co.uk>
2017-11-04 18:17:57 +0900

    tests/sources/patch.py: Converted to use CLI fixtures

--- tests/sources/patch.py
+++ tests/sources/patch.py
@@ -1,10 +1,9 @@
 import os
 import pytest
 
+from buildstream._pipeline import PipelineError
 from buildstream import SourceError
-
-# import our common fixture
-from .fixture import Setup
+from tests.testutils import cli
 
 DATA_DIR = os.path.join(
     os.path.dirname(os.path.realpath(__file__)),
@@ -13,124 +12,104 @@ DATA_DIR = os.path.join(
 
 
 @pytest.mark.datafiles(os.path.join(DATA_DIR, 'basic'))
-def test_create_source(tmpdir, datafiles):
-    setup = Setup(datafiles, 'target.bst', tmpdir)
-    patch_sources = [source for source in setup.sources if source.get_kind() == 'patch']
-    assert(len(patch_sources) == 1)
+def test_missing_patch(cli, tmpdir, datafiles):
+    project = os.path.join(datafiles.dirname, datafiles.basename)
 
-
-@pytest.mark.datafiles(os.path.join(DATA_DIR, 'basic'))
-def test_preflight(tmpdir, datafiles):
-    setup = Setup(datafiles, 'target.bst', tmpdir)
-    patch_source = [source for source in setup.sources if source.get_kind() == 'patch'][0]
-
-    # Just expect that this passes without throwing any exception
-    patch_source.preflight()
-
-
-@pytest.mark.datafiles(os.path.join(DATA_DIR, 'basic'))
-def test_preflight_fail(tmpdir, datafiles):
-    setup = Setup(datafiles, 'target.bst', tmpdir)
-    patch_source = [source for source in setup.sources if source.get_kind() == 'patch'][0]
-
-    # Delete the file which the local source wants
+    # Removing the local file causes preflight to fail
     localfile = os.path.join(datafiles.dirname, datafiles.basename, 'file_1.patch')
     os.remove(localfile)
 
-    # Expect a preflight error
-    with pytest.raises(SourceError):
-        patch_source.preflight()
+    result = cli.run(project=project, args=[
+        'show', 'target.bst'
+    ])
+    assert result.exit_code != 0
+    assert result.exception
+    assert isinstance(result.exception, SourceError)
 
 
 @pytest.mark.datafiles(os.path.join(DATA_DIR, 'basic'))
-def test_unique_key(tmpdir, datafiles):
-    setup = Setup(datafiles, 'target.bst', tmpdir)
-    patch_source = [source for source in setup.sources if source.get_kind() == 'patch'][0]
+def test_stage_and_patch(cli, tmpdir, datafiles):
+    project = os.path.join(datafiles.dirname, datafiles.basename)
+    checkoutdir = os.path.join(str(tmpdir), "checkout")
 
-    # Get the unique key
-    unique_key = patch_source.get_unique_key()
+    # Build, checkout
+    result = cli.run(project=project, args=['build', 'target.bst'])
+    assert result.exit_code == 0
+    result = cli.run(project=project, args=['checkout', 'target.bst', checkoutdir])
+    assert result.exit_code == 0
 
-    # No easy way to test this, let's just check that the
-    # returned 'thing' is an array of tuples and the first element
-    # of the first tuple is the filename, and the second is not falsy
-    assert(isinstance(unique_key, list))
-    filename, digest, strip_level = unique_key
-    assert(filename == 'file_1.patch')
-    assert(digest)
-    assert(strip_level == 1)
-
-
-@pytest.mark.datafiles(os.path.join(DATA_DIR, 'basic'))
-def test_stage_file(tmpdir, datafiles):
-    setup = Setup(datafiles, 'target.bst', tmpdir)
-
-    for source in setup.sources:
-        source.preflight()
-        source.stage(setup.context.builddir)
-    with open(os.path.join(setup.context.builddir, 'file.txt')) as f:
+    # Test the file.txt was patched and changed
+    with open(os.path.join(checkoutdir, 'file.txt')) as f:
         assert(f.read() == 'This is text file with superpowers\n')
 
 
 @pytest.mark.datafiles(os.path.join(DATA_DIR, 'basic'))
-def test_stage_file_nonexistent_dir(tmpdir, datafiles):
-    setup = Setup(datafiles, 'failure-nonexistent-dir.bst', tmpdir)
-    patch_sources = [source for source in setup.sources if source.get_kind() == 'patch']
-    assert(len(patch_sources) == 1)
+def test_stage_file_nonexistent_dir(cli, tmpdir, datafiles):
+    project = os.path.join(datafiles.dirname, datafiles.basename)
+    checkoutdir = os.path.join(str(tmpdir), "checkout")
 
-    for source in setup.sources:
-        source.preflight()
-        if source.get_kind() == 'patch':
-            with pytest.raises(SourceError):
-                source.stage(setup.context.builddir)
+    # Fails at build time because it tries to patch into a non-existing directory
+    result = cli.run(project=project, args=['build', 'failure-nonexistent-dir.bst'])
+    assert result.exit_code != 0
+    assert result.exception
+    assert isinstance(result.exception, PipelineError)
 
 
 @pytest.mark.datafiles(os.path.join(DATA_DIR, 'basic'))
-def test_stage_file_empty_dir(tmpdir, datafiles):
-    setup = Setup(datafiles, 'failure-empty-dir.bst', tmpdir)
-    patch_sources = [source for source in setup.sources if source.get_kind() == 'patch']
-    assert(len(patch_sources) == 1)
+def test_stage_file_empty_dir(cli, tmpdir, datafiles):
+    project = os.path.join(datafiles.dirname, datafiles.basename)
+    checkoutdir = os.path.join(str(tmpdir), "checkout")
 
-    for source in setup.sources:
-        source.preflight()
-        if source.get_kind() == 'patch':
-            with pytest.raises(SourceError):
-                source.stage(setup.context.builddir)
+    # Fails at build time because it tries to patch with nothing else staged
+    result = cli.run(project=project, args=['build', 'failure-empty-dir.bst'])
+    assert result.exit_code != 0
+    assert result.exception
+    assert isinstance(result.exception, PipelineError)
 
 
 @pytest.mark.datafiles(os.path.join(DATA_DIR, 'separate-patch-dir'))
-def test_stage_separate_patch_dir(tmpdir, datafiles):
-    setup = Setup(datafiles, 'target.bst', tmpdir)
-    patch_sources = [source for source in setup.sources if source.get_kind() == 'patch']
-    assert(len(patch_sources) == 1)
+def test_stage_separate_patch_dir(cli, tmpdir, datafiles):
+    project = os.path.join(datafiles.dirname, datafiles.basename)
+    checkoutdir = os.path.join(str(tmpdir), "checkout")
 
-    for source in setup.sources:
-        source.preflight()
-        source.stage(setup.context.builddir)
-    with open(os.path.join(setup.context.builddir, 'file.txt')) as f:
+    # Track, fetch, build, checkout
+    result = cli.run(project=project, args=['build', 'target.bst'])
+    assert result.exit_code == 0
+    result = cli.run(project=project, args=['checkout', 'target.bst', checkoutdir])
+    assert result.exit_code == 0
+
+    # Test the file.txt was patched and changed
+    with open(os.path.join(checkoutdir, 'test-dir', 'file.txt')) as f:
         assert(f.read() == 'This is text file in a directory with superpowers\n')
 
 
 @pytest.mark.datafiles(os.path.join(DATA_DIR, 'multiple-patches'))
-def test_stage_multiple_patches(tmpdir, datafiles):
-    setup = Setup(datafiles, 'target.bst', tmpdir)
-    patch_sources = [source for source in setup.sources if source.get_kind() == 'patch']
-    assert(len(patch_sources) == 2)
+def test_stage_multiple_patches(cli, tmpdir, datafiles):
+    project = os.path.join(datafiles.dirname, datafiles.basename)
+    checkoutdir = os.path.join(str(tmpdir), "checkout")
 
-    for source in setup.sources:
-        source.preflight()
-        source.stage(setup.context.builddir)
-    with open(os.path.join(setup.context.builddir, 'file.txt')) as f:
+    # Track, fetch, build, checkout
+    result = cli.run(project=project, args=['build', 'target.bst'])
+    assert result.exit_code == 0
+    result = cli.run(project=project, args=['checkout', 'target.bst', checkoutdir])
+    assert result.exit_code == 0
+
+    # Test the file.txt was patched and changed
+    with open(os.path.join(checkoutdir, 'file.txt')) as f:
         assert(f.read() == 'This is text file with more superpowers\n')
 
 
 @pytest.mark.datafiles(os.path.join(DATA_DIR, 'different-strip-level'))
-def test_patch_strip_level(tmpdir, datafiles):
-    setup = Setup(datafiles, 'target.bst', tmpdir)
-    patch_sources = [source for source in setup.sources if source.get_kind() == 'patch']
-    assert(len(patch_sources) == 1)
+def test_patch_strip_level(cli, tmpdir, datafiles):
+    project = os.path.join(datafiles.dirname, datafiles.basename)
+    checkoutdir = os.path.join(str(tmpdir), "checkout")
 
-    for source in setup.sources:
-        source.preflight()
-        source.stage(setup.context.builddir)
-    with open(os.path.join(setup.context.builddir, 'file.txt')) as f:
+    # Track, fetch, build, checkout
+    result = cli.run(project=project, args=['build', 'target.bst'])
+    assert result.exit_code == 0
+    result = cli.run(project=project, args=['checkout', 'target.bst', checkoutdir])
+    assert result.exit_code == 0
+
+    # Test the file.txt was patched and changed
+    with open(os.path.join(checkoutdir, 'file.txt')) as f:
         assert(f.read() == 'This is text file with superpowers\n')
