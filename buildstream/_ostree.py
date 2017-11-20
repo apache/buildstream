apache/buildstream

commit 3b17762a4cab23c238762ca32baa348788347473

Switch old-style string formattings to new '.format()'

buildstream/_ostree.py
@@ -271,7 +271,8 @@ def fetch(repo, remote="origin", ref=None, progress=None):
             else:
                 percent = (fetched * 1.0 / requested) * 100
 
-            progress(percent, "Receiving objects: %d%% (%d/%d) %s" % (percent, fetched, requested, formatted_bytes))
+            progress(percent,
+                     "Receiving objects: {:d}% ({:d}/{:d}) {}".format(percent, fetched, requested, formatted_bytes))
         else:
             progress(100.0, "Writing Objects")
 

buildstream/_artifactcache/pushreceive.py
@@ -67,8 +67,8 @@ def msg_byteorder(sys_byteorder=sys.byteorder):
     elif sys_byteorder == 'big':
         return 'B'
     else:
-        raise PushException('Unrecognized system byteorder %s'
-                            % sys_byteorder)
+        raise PushException('Unrecognized system byteorder {}'
+                            .format(sys_byteorder))
 
 
 def sys_byteorder(msg_byteorder):
@@ -77,8 +77,8 @@ def sys_byteorder(msg_byteorder):
     elif msg_byteorder == 'B':
         return 'big'
     else:
-        raise PushException('Unrecognized message byteorder %s'
-                            % msg_byteorder)
+        raise PushException('Unrecognized message byteorder {}'
+                            .format(msg_byteorder))
 
 
 def ostree_object_path(repo, obj):
@@ -220,18 +220,18 @@ class PushMessageReader(object):
 
     def decode_header(self, header):
         if len(header) != HEADER_SIZE:
-            raise Exception('Header is %d bytes, not %d' % (len(header), HEADER_SIZE))
+            raise Exception('Header is {:d} bytes, not {:d}'.format(len(header), HEADER_SIZE))
         order = sys_byteorder(chr(header[0]))
         version = int(header[1])
         if version != PROTO_VERSION:
-            raise Exception('Unsupported protocol version %d' % version)
+            raise Exception('Unsupported protocol version {:d}'.format(version))
         cmdtype = PushCommandType(int(header[2]))
         vlen = int.from_bytes(header[3:], order)
         return order, version, cmdtype, vlen
 
     def decode_message(self, message, size, order):
         if len(message) != size:
-            raise Exception('Expected %d bytes, but got %d' % (size, len(message)))
+            raise Exception('Expected {:d} bytes, but got {:d}'.format(size, len(message)))
         data = GLib.Bytes.new(message)
         variant = GLib.Variant.new_from_bytes(GLib.VariantType.new('a{sv}'),
                                               data, False)
@@ -317,7 +317,7 @@ def parse_remote_location(remotepath, remote_port):
     if url.netloc:
         if url.scheme != 'ssh':
             raise PushException('Only URL scheme ssh is allowed, '
-                                'not "%s"' % url.scheme)
+                                'not "{}"'.format(url.scheme))
         remote_host = url.hostname
         remote_user = url.username
         remote_repo = url.path
@@ -333,9 +333,9 @@ def parse_remote_location(remotepath, remote_port):
             remainder = parts[0]
         parts = remainder.split(':', 1)
         if len(parts) != 2:
-            raise PushException('Remote repository "%s" does not '
+            raise PushException('Remote repository "{}" does not '
                                 'contain a hostname and path separated '
-                                'by ":"' % remotepath)
+                                'by ":"'.format(remotepath))
         remote_host, remote_repo = parts
 
     return remote_host, remote_user, remote_repo, remote_port
@@ -407,15 +407,15 @@ class OSTreePusher(object):
             _, commit = self.repo.load_variant_if_exists(OSTree.ObjectType.COMMIT,
                                                          parent)
             if commit is None:
-                raise PushException('Shallow history from commit %s does '
-                                    'not contain remote commit %s' % (local, remote))
+                raise PushException('Shallow history from commit {} does '
+                                    'not contain remote commit {}'.format(local, remote))
             parent = OSTree.commit_get_parent(commit)
             if parent is None:
                 break
         if remote is not None and parent != remote:
             self.writer.send_done()
-            raise PushExistsException('Remote commit %s not descendent of '
-                                      'commit %s' % (remote, local))
+            raise PushExistsException('Remote commit {} not descendent of '
+                                      'commit {}'.format(remote, local))
 
     def needed_objects(self, commits):
         objects = set()

buildstream/_frontend/main.py
@@ -497,7 +497,7 @@ def shell(app, element, sysroot, build, command):
         sys.exit(exitcode)
     except BstError as e:
         click.echo("")
-        click.echo("Errors shelling into this pipeline: %s" % str(e))
+        click.echo("Errors shelling into this pipeline: {}".format(str(e)))
         sys.exit(-1)
 
 
@@ -773,7 +773,7 @@ class App():
             self.context = Context(self.main_options['option'])
             self.context.load(config)
         except BstError as e:
-            click.echo("Error loading user configuration: %s" % str(e))
+            click.echo("Error loading user configuration: {}".format(str(e)))
             sys.exit(-1)
 
         # Override things in the context from our command line options,
@@ -831,7 +831,7 @@ class App():
         try:
             self.project = Project(directory, self.context)
         except BstError as e:
-            click.echo("Error loading project: %s" % str(e))
+            click.echo("Error loading project: {}".format(str(e)))
             sys.exit(-1)
 
         try:
@@ -844,7 +844,7 @@ class App():
                                      remote_ticker=self.remote_ticker,
                                      cache_ticker=self.cache_ticker)
         except BstError as e:
-            click.echo("Error loading pipeline: %s" % str(e))
+            click.echo("Error loading pipeline: {}".format(str(e)))
             sys.exit(-1)
 
         # Create our status printer, only available in interactive

buildstream/_fuse/fuse.py
@@ -268,7 +268,7 @@ elif _system == 'Linux':
             ('st_ctimespec', c_timespec),
             ('st_ino', c_ulonglong)]
 else:
-    raise NotImplementedError('%s is not supported.' % _system)
+    raise NotImplementedError('{} is not supported.'.format(_system))
 
 
 class c_statvfs(Structure):
@@ -507,7 +507,7 @@ class FUSE(object):
             if isinstance(value, bool):
                 if value is True: yield key
             else:
-                yield '%s=%s' % (key, value)
+                yield '{}={}'.format(key, value)
 
     @staticmethod
     def _wrapper(func, *args, **kwargs):
@@ -608,7 +608,7 @@ class FUSE(object):
 
         retsize = len(ret)
         assert retsize <= size, \
-            'actual amount read %d greater than expected %d' % (retsize, size)
+            'actual amount read {:d} greater than expected {:d}'.format(retsize, size)
 
         data = create_string_buffer(ret, retsize)
         memmove(buf, data, retsize)

buildstream/_loader.py
@@ -154,8 +154,8 @@ def extract_depends_from_node(owner, data):
             elif dep_type not in [Symbol.BUILD, Symbol.RUNTIME]:
                 provenance = _yaml.node_get_provenance(dep, key=Symbol.TYPE)
                 raise LoadError(LoadErrorReason.INVALID_DATA,
-                                "%s: Dependency type '%s' is not 'build', 'runtime' or 'all'" %
-                                (str(provenance), dep_type))
+                                "{}: Dependency type '{}' is not 'build', 'runtime' or 'all'"
+                                .format(str(provenance), dep_type))
 
             filename = _yaml.node_get(dep, str, Symbol.FILENAME)
             dependency = Dependency(owner, filename, filename=filename,
@@ -166,8 +166,8 @@ def extract_depends_from_node(owner, data):
             provenance = _yaml.node_get_provenance(data, key=Symbol.DEPENDS, indices=[index])
 
             raise LoadError(LoadErrorReason.INVALID_DATA,
-                            "%s: List '%s' element %d is not a list or dict" %
-                            (str(provenance), Symbol.DEPENDS, index))
+                            "{}: List '{}' element {:d} is not a list or dict"
+                            .format(str(provenance), Symbol.DEPENDS, index))
 
         output_deps.append(dependency)
 
@@ -198,9 +198,9 @@ class Loader():
                 # XXX Should this just be an assertion ?
                 # Expect that the caller gives us the right thing at least ?
                 raise LoadError(LoadErrorReason.INVALID_DATA,
-                                "Target '%s' was not specified as a relative "
-                                "path to the base project directory: %s" %
-                                (filename, basedir))
+                                "Target '{}' was not specified as a relative "
+                                "path to the base project directory: {}"
+                                .format(filename, basedir))
 
         self.options = options   # Project options (OptionPool)
         self.basedir = basedir   # Base project directory
@@ -314,8 +314,8 @@ class Loader():
 
         if check_elements.get(element_name) is not None:
             raise LoadError(LoadErrorReason.CIRCULAR_DEPENDENCY,
-                            "Circular dependency detected for element: %s" %
-                            element.filename)
+                            "Circular dependency detected for element: {}"
+                            .format(element.filename))
 
         # Push / Check each dependency / Pop
         check_elements[element_name] = True

buildstream/_options/optionpool.py
@@ -138,7 +138,7 @@ class OptionPool():
         # Variables must be resolved at this point.
         #
         try:
-            template_string = "{%% if %s %%} True {%% else %%} False {%% endif %%}" % expression
+            template_string = "{{% if {} %}} True {{% else %}} False {{% endif %}}".format(expression)
             template = self.environment.from_string(template_string)
             context = template.new_context(self.variables, shared=True)
             result = template.root_render_func(context)

buildstream/_variables.py
@@ -159,7 +159,7 @@ class Variables():
                         summary += line.format(unmatched=unmatch, variable=var, provenance=provenance)
 
                 raise LoadError(LoadErrorReason.UNRESOLVED_VARIABLE,
-                                "Failed to resolve one or more variable:\n%s" % summary)
+                                "Failed to resolve one or more variable:\n{}".format(summary))
 
             last_unmatched = unmatched
 

buildstream/_yaml.py
@@ -53,7 +53,7 @@ class Provenance():
 
     # Convert a Provenance to a string for error reporting
     def __str__(self):
-        return "%s [line %d column %d]" % (self.filename, self.line, self.col)
+        return "{} [line {:d} column {:d}]".format(self.filename, self.line, self.col)
 
     # Abstract method
     def clone(self):
@@ -152,9 +152,9 @@ class CompositeTypeError(CompositeError):
     def __init__(self, path, expected_type, actual_type):
         super(CompositeTypeError, self).__init__(
             path,
-            "Error compositing dictionary key '%s', expected source type '%s' "
-            "but received type '%s'" %
-            (path, expected_type.__name__, actual_type.__name__))
+            "Error compositing dictionary key '{}', expected source type '{}' "
+            "but received type '{}'"
+            .format(path, expected_type.__name__, actual_type.__name__))
         self.expected_type = expected_type
         self.actual_type = actual_type
 
@@ -180,7 +180,7 @@ def load(filename, shortname=None, copy_tree=False):
             return load_data(f, shortname=shortname, copy_tree=copy_tree)
     except FileNotFoundError as e:
         raise LoadError(LoadErrorReason.MISSING_FILE,
-                        "Could not find file at %s" % filename) from e
+                        "Could not find file at {}".format(filename)) from e
 
 
 # Like load(), but doesnt require the data to be in a file
@@ -191,7 +191,7 @@ def load_data(data, shortname=None, copy_tree=False):
         contents = yaml.load(data, yaml.loader.RoundTripLoader)
     except (yaml.scanner.ScannerError, yaml.composer.ComposerError, yaml.parser.ParserError) as e:
         raise LoadError(LoadErrorReason.INVALID_YAML,
-                        "Malformed YAML:\n\n%s\n\n%s\n" % (e.problem, e.problem_mark)) from e
+                        "Malformed YAML:\n\n{}\n\n{}\n".format(e.problem, e.problem_mark)) from e
 
     if not isinstance(contents, dict):
         # Special case allowance for None, when the loaded file has only comments in it.
@@ -199,8 +199,8 @@ def load_data(data, shortname=None, copy_tree=False):
             contents = {}
         else:
             raise LoadError(LoadErrorReason.INVALID_YAML,
-                            "YAML file has content of type '%s' instead of expected type 'dict': %s" %
-                            (type(contents).__name__, shortname))
+                            "YAML file has content of type '{}' instead of expected type 'dict': {}"
+                            .format(type(contents).__name__, shortname))
 
     return node_decorated_copy(shortname, contents, copy_tree=copy_tree)
 
@@ -326,7 +326,7 @@ def node_get(node, expected_type, key, indices=[], default_value=None):
     provenance = node_get_provenance(node)
     if value is None:
         raise LoadError(LoadErrorReason.INVALID_DATA,
-                        "%s: Dictionary did not contain expected key '%s'" % (str(provenance), key))
+                        "{}: Dictionary did not contain expected key '{}'".format(str(provenance), key))
 
     path = key
     if indices:
@@ -334,7 +334,7 @@ def node_get(node, expected_type, key, indices=[], default_value=None):
         value = node_get(node, list, key)
         for index in indices:
             value = value[index]
-            path += '[%d]' % index
+            path += '[{:d}]'.format(index)
 
     if not isinstance(value, expected_type):
         # Attempt basic conversions if possible, typically we want to
@@ -359,8 +359,8 @@ def node_get(node, expected_type, key, indices=[], default_value=None):
         except (ValueError, TypeError):
             provenance = node_get_provenance(node, key=key, indices=indices)
             raise LoadError(LoadErrorReason.INVALID_DATA,
-                            "%s: Value of '%s' is not of the expected type '%s'" %
-                            (str(provenance), path, expected_type.__name__))
+                            "{}: Value of '{}' is not of the expected type '{}'"
+                            .format(str(provenance), path, expected_type.__name__))
 
     # Trim it at the bud, let all loaded strings from yaml be stripped of whitespace
     if isinstance(value, str):
@@ -757,11 +757,11 @@ def composite(target, source):
         if source_provenance:
             error_prefix = "{}: ".format(str(source_provenance))
         raise LoadError(LoadErrorReason.ILLEGAL_COMPOSITE,
-                        "%sExpected '%s' type for configuration '%s', instead received '%s'" %
-                        (error_prefix,
-                         e.expected_type.__name__,
-                         e.path,
-                         e.actual_type.__name__)) from e
+                        "{}Expected '{}' type for configuration '{}', instead received '{}'"
+                        .format(error_prefix,
+                                e.expected_type.__name__,
+                                e.path,
+                                e.actual_type.__name__)) from e
 
 
 # SanitizedDict is an OrderedDict that is dumped as unordered mapping.

commit b9418b495ca5b12953c97724d8c10f2ac8db5767

_ostree.py: Fix string formatting in fetch()
New-style formatting does not implicitly convert from float to int. Add
explicit conversion to fix the following error:

	ValueError: Unknown format code 'd' for object of type 'float'

Fixes: 3b17762a ("Switch old-style string formattings to new...")

buildstream/_ostree.py
@@ -272,7 +272,8 @@ def fetch(repo, remote="origin", ref=None, progress=None):
                 percent = (fetched * 1.0 / requested) * 100
 
             progress(percent,
-                     "Receiving objects: {:d}% ({:d}/{:d}) {}".format(percent, fetched, requested, formatted_bytes))
+                     "Receiving objects: {:d}% ({:d}/{:d}) {}".format(int(percent), fetched,
+                                                                      requested, formatted_bytes))
         else:
             progress(100.0, "Writing Objects")
 