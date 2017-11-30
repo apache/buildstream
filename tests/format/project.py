1ea5423f92c6f7b2c982a74c6c70d56d14417bfe
Jonathan Maw <jonathan.maw@codethink.co.uk>
2017-11-30 17:54:43 +0000

    tests: Test whether a plugin is allowed to be loaded

--- tests/format/project.py
+++ tests/format/project.py
@@ -1,7 +1,7 @@
 import os
 import pytest
 from buildstream import _yaml
-from buildstream._exceptions import LoadError, LoadErrorReason
+from buildstream._exceptions import LoadError, LoadErrorReason, PluginError
 from tests.testutils.runcli import cli
 
 
@@ -10,6 +10,24 @@ DATA_DIR = os.path.join(
     os.path.dirname(os.path.realpath(__file__)),
     "project"
 )
+
+
+@pytest.mark.datafiles(DATA_DIR)
+def test_project_plugin_load_allowed(cli, datafiles):
+    project = os.path.join(datafiles.dirname, datafiles.basename, 'plugin-allowed')
+    result = cli.run(project=project, silent=True, args=[
+        'show', 'element.bst'])
+    assert result.exit_code == 0
+
+
+@pytest.mark.datafiles(DATA_DIR)
+def test_project_plugin_load_forbidden(cli, datafiles):
+    project = os.path.join(datafiles.dirname, datafiles.basename, 'plugin-forbidden')
+    result = cli.run(project=project, silent=True, args=[
+        'show', 'element.bst'])
+    assert result.exit_code != 0
+    assert result.exception
+    assert isinstance(result.exception, PluginError)
 
 
 @pytest.mark.datafiles(DATA_DIR)
