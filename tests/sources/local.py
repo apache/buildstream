5084e66277d57398d9feb4f161fa8e114968cbe1
Tristan Van Berkom <tristan.vanberkom@codethink.co.uk>
2018-01-02 17:37:19 +0900

    tests/sources/local.py: Adding test for staging a symlink.
    This has remained without test coverage mostly due to upstream
    pytest-datafiles bug https://github.com/omarkohl/pytest-datafiles/issues/1

--- tests/sources/local.py
+++ tests/sources/local.py
@@ -55,6 +55,32 @@ def test_stage_directory(cli, tmpdir, datafiles):
     assert(os.path.exists(os.path.join(checkoutdir, 'subdir', 'anotherfile.txt')))
 
 
+@pytest.mark.datafiles(os.path.join(DATA_DIR, 'symlink'))
+def test_stage_symlink(cli, tmpdir, datafiles):
+
+    project = os.path.join(datafiles.dirname, datafiles.basename)
+    checkoutdir = os.path.join(str(tmpdir), "checkout")
+
+    # Workaround datafiles bug:
+    #
+    #   https://github.com/omarkohl/pytest-datafiles/issues/1
+    #
+    # Create the symlink by hand.
+    symlink = os.path.join(project, 'files', 'symlink-to-file.txt')
+    os.symlink('file.txt', symlink)
+
+    # Build, checkout
+    result = cli.run(project=project, args=['build', 'target.bst'])
+    result.assert_success()
+    result = cli.run(project=project, args=['checkout', 'target.bst', checkoutdir])
+    result.assert_success()
+
+    # Check that the checkout contains the expected file and directory and other file
+    assert(os.path.exists(os.path.join(checkoutdir, 'file.txt')))
+    assert(os.path.exists(os.path.join(checkoutdir, 'symlink-to-file.txt')))
+    assert(os.path.islink(os.path.join(checkoutdir, 'symlink-to-file.txt')))
+
+
 @pytest.mark.datafiles(os.path.join(DATA_DIR, 'file-exists'))
 def test_stage_file_exists(cli, tmpdir, datafiles):
     project = os.path.join(datafiles.dirname, datafiles.basename)
