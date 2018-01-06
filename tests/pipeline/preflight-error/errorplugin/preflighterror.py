60d5e070c17ef15caa42a847b0f9957432bd6468
Tristan Van Berkom <tristan.vanberkom@codethink.co.uk>
2018-01-06 18:39:05 +0900

    tests/pipeline/preflight.py: Added test to ensure graceful exit at preflight error time

---
+++ tests/pipeline/preflight-error/errorplugin/preflighterror.py
@@ -0,0 +1,35 @@
+from buildstream import Source, SourceError, Consistency
+
+
+class PreflightErrorSource(Source):
+
+    def configure(self, node):
+        pass
+
+    def preflight(self):
+
+        # Raise a preflight error unconditionally
+        raise SourceError("{}: Unsatisfied requirements in preflight, raising this error",
+                          reason="the-preflight-error")
+
+    def get_unique_key(self):
+        return {}
+
+    def get_consistency(self):
+        return Consistency.CACHED
+
+    def get_ref(self):
+        return None
+
+    def set_ref(self, ref, node):
+        pass
+
+    def fetch(self):
+        pass
+
+    def stage(self, directory):
+        pass
+
+
+def setup():
+    return PreflightErrorSource
