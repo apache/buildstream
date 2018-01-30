aec3ccde6f13f1945eec3933284ca32e2d7a2a53
Sam Thursfield <sam.thursfield@codethink.co.uk>
2018-01-30 16:50:39 +0000

    Update copyright years of changed files

--- buildstream/plugins/elements/meson.py
+++ buildstream/plugins/elements/meson.py
@@ -1,4 +1,5 @@
 #  Copyright (C) 2017 Patrick Griffis
+#  Copyright (C) 2018 Codethink Ltd.
 #
 #  This program is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU Lesser General Public
