ecef1319bcd0e10b6ff17019cc298e2d3e5ce880
Jürg Billeter <j@bitron.ch>
2018-02-13 05:48:32 +0100

    source.py: Fix typo in workspace warning
    Fixes #250

--- buildstream/source.py
+++ buildstream/source.py
@@ -420,7 +420,7 @@ class Source(Plugin):
             if self._has_workspace():
                 detail = "This source has an open workspace.\n" \
                     + "To start using the new reference, please close the existing workspace."
-                self.warn("Updated reference will be ingnored as source has open workspace", detail=detail)
+                self.warn("Updated reference will be ignored as source has open workspace", detail=detail)
 
         return new_ref
 
