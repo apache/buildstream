83a18f9856b346f2686fcf8857a4458b312137e9
Tristan Van Berkom <tristan.vanberkom@codethink.co.uk>
2018-03-18 21:17:46 +0900

    tests/format/project.py: Test that conditional statements are supported in project.refs

--- tests/format/project.py
+++ tests/format/project.py
@@ -34,3 +34,30 @@ def test_project_conf_duplicate_plugins(cli, datafiles):
     result = cli.run(project=project, silent=True, args=[
         'show', 'element.bst'])
     result.assert_main_error(ErrorDomain.LOAD, LoadErrorReason.INVALID_YAML)
+
+
+# Assert that we get a different cache key for target.bst, depending
+# on a conditional statement we have placed in the project.refs file.
+#
+@pytest.mark.datafiles(DATA_DIR)
+def test_project_refs_options(cli, datafiles):
+    project = os.path.join(datafiles.dirname, datafiles.basename, 'refs-options')
+
+    result1 = cli.run(project=project, silent=True, args=[
+        '--option', 'test', 'True',
+        'show',
+        '--deps', 'none',
+        '--format', '%{key}',
+        'target.bst'])
+    result1.assert_success()
+
+    result2 = cli.run(project=project, silent=True, args=[
+        '--option', 'test', 'False',
+        'show',
+        '--deps', 'none',
+        '--format', '%{key}',
+        'target.bst'])
+    result2.assert_success()
+
+    # Assert that the cache keys are different
+    assert result1.output != result2.output
