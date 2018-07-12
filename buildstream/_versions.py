73f0aacebe28f7945f1008a2d8c59866d7a7ffd8
Ed Baunton <ebaunton1@bloomberg.net>
2018-07-12 18:11:08 -0400

    Add remote source plugin
    Add a plugin that supports downloading files verbatim from a source with
    an optional overridable filename and destination directory. Bumps bst
    format version to 10.
    
    Fixes #163

--- buildstream/_versions.py
+++ buildstream/_versions.py
@@ -23,7 +23,7 @@
 # This version is bumped whenever enhancements are made
 # to the `project.conf` format or the core element format.
 #
-BST_FORMAT_VERSION = 9
+BST_FORMAT_VERSION = 10
 
 
 # The base BuildStream artifact version
