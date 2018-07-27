3d26ff6eff05648afdc3188175bd327c6f1676b1
Ed Baunton <ebaunton1@bloomberg.net>
2018-07-27 14:01:57 +0100

    remote.py: Add support for marking downloaded files executable
    Add an optional flag to make files executable after having downloaded them.
    
    Instead of leaving the permissioning of downloaded file in remote.py up
    to the user's umask; expressly set permissions to 0644 or 0755 if
    executable.
    
    Bump format version to 13.

--- buildstream/plugins/sources/remote.py
+++ buildstream/plugins/sources/remote.py
@@ -35,6 +35,10 @@ remote - stage files from remote urls
    # If not specified, the basename of the url will be used.
    # filename: customfilename
 
+   # Optionally specify whether the downloaded file should be
+   # marked executable.
+   # executable: true
+
    # Specify the url. Using an alias defined in your project
    # configuration is encouraged. 'bst track' will update the
    # sha256sum in 'ref' to the downloaded file's sha256sum.
@@ -42,6 +46,8 @@ remote - stage files from remote urls
 
    # Specify the ref. It's a sha256sum of the file you download.
    ref: 6c9f6f68a131ec6381da82f2bff978083ed7f4f7991d931bfa767b7965ebc94b
+
+
 
 .. note::
 
@@ -60,21 +66,30 @@ class RemoteSource(DownloadableFileSource):
         super().configure(node)
 
         self.filename = self.node_get_member(node, str, 'filename', os.path.basename(self.url))
+        self.executable = self.node_get_member(node, bool, 'executable', False)
 
         if os.sep in self.filename:
             raise SourceError('{}: filename parameter cannot contain directories'.format(self),
                               reason="filename-contains-directory")
-        self.node_validate(node, DownloadableFileSource.COMMON_CONFIG_KEYS + ['filename'])
+        self.node_validate(node, DownloadableFileSource.COMMON_CONFIG_KEYS + ['filename', 'executable'])
 
     def get_unique_key(self):
-        return super().get_unique_key() + [self.filename]
+        return super().get_unique_key() + [self.filename, self.executable]
 
     def stage(self, directory):
         # Same as in local plugin, don't use hardlinks to stage sources, they
         # are not write protected in the sandbox.
         dest = os.path.join(directory, self.filename)
         with self.timed_activity("Staging remote file to {}".format(dest)):
+
             utils.safe_copy(self._get_mirror_file(), dest)
+
+            # To prevent user's umask introducing variability here, explicitly set
+            # file modes.
+            if self.executable:
+                os.chmod(dest, 0o755)
+            else:
+                os.chmod(dest, 0o644)
 
 
 def setup():
