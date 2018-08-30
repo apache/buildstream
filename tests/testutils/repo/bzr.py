680e4fe1d3d3b48a7f628d980cceacf52c4795df
knownexus <phillip.smyth@codethink.co.uk>
2018-08-30 14:37:55 +0100

    Replacing string 'bzr' with value from host tools

--- tests/testutils/repo/bzr.py
+++ tests/testutils/repo/bzr.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import pytest
 
+from buildstream import utils
 from .repo import Repo
 from ..site import HAVE_BZR
 
@@ -16,15 +17,16 @@ class Bzr(Repo):
         if not HAVE_BZR:
             pytest.skip("bzr is not available")
         super(Bzr, self).__init__(directory, subdir)
+        self.bzr = utils.get_host_tool('bzr')
 
     def create(self, directory):
         branch_dir = os.path.join(self.repo, 'trunk')
 
-        subprocess.call(['bzr', 'init-repo', self.repo], env=BZR_ENV)
-        subprocess.call(['bzr', 'init', branch_dir], env=BZR_ENV)
+        subprocess.call([self.bzr, 'init-repo', self.repo], env=BZR_ENV)
+        subprocess.call([self.bzr, 'init', branch_dir], env=BZR_ENV)
         self.copy_directory(directory, branch_dir)
-        subprocess.call(['bzr', 'add', '.'], env=BZR_ENV, cwd=branch_dir)
-        subprocess.call(['bzr', 'commit', '--message="Initial commit"'],
+        subprocess.call([self.bzr, 'add', '.'], env=BZR_ENV, cwd=branch_dir)
+        subprocess.call([self.bzr, 'commit', '--message="Initial commit"'],
                         env=BZR_ENV, cwd=branch_dir)
 
         return self.latest_commit()
@@ -42,7 +44,7 @@ class Bzr(Repo):
 
     def latest_commit(self):
         output = subprocess.check_output([
-            'bzr', 'version-info',
+            self.bzr, 'version-info',
             '--custom', '--template={revno}',
             os.path.join(self.repo, 'trunk')
         ], env=BZR_ENV)
