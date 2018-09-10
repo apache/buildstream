5e7cc645c69f96b35508113fdd311ce2e8ba9ab2
William Salmon <will.salmon@codethink.co.uk>
2018-09-10 15:37:02 +0100

    Add conf-root variable to builds
    Adding the conf-root variable makes creating out of source builds
    slightly easier.
    
    For issue #512 in Gitlab.

--- buildstream/_versions.py
+++ buildstream/_versions.py
@@ -23,7 +23,7 @@
 # This version is bumped whenever enhancements are made
 # to the `project.conf` format or the core element format.
 #
-BST_FORMAT_VERSION = 16
+BST_FORMAT_VERSION = 17
 
 
 # The base BuildStream artifact version
