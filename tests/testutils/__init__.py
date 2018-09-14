12296c8460b6246d287fc1e010405b9f13709a34
Tristan Van Berkom <tristan.vanberkom@codethink.co.uk>
2018-09-14 18:47:35 +0900

    testutils/element_generators.py: Changing sized element functions
     * create_element_size()
    
       Now uses a git Repo object instead of a local source, and
       returns the repo.
    
     * update_element_size()
    
       Added this function which can now resize the expected output
       of an element generated with create_element_size(), useful
       to allow testing sized elements with the tracking feature.

--- tests/testutils/__init__.py
+++ tests/testutils/__init__.py
@@ -1,5 +1,5 @@
 from .runcli import cli, cli_integration
 from .repo import create_repo, ALL_REPO_KINDS
 from .artifactshare import create_artifact_share
-from .element_generators import create_element_size
+from .element_generators import create_element_size, update_element_size
 from .junction import generate_junction
