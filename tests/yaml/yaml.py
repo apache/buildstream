ae5b554b5762cd29e5818da155721f37a869447d
Lachlan Mackenzie <lachlan.mackenzie@codethink.co.uk>
2018-10-05 17:32:40 +0100

    Add loader for yaml test files cache option

--- tests/yaml/yaml.py
+++ tests/yaml/yaml.py
@@ -1,5 +1,6 @@
 import os
 import pytest
+import tempfile
 from collections import Mapping
 
 from buildstream import _yaml
@@ -148,6 +149,22 @@ def test_composite_preserve_originals(datafiles):
 
     # But the original node is not effected by the override.
     assert(_yaml.node_get(orig_extra, str, 'old') == 'new')
+
+
+def load_yaml_file(filename, *, cache_path, shortname=None, from_cache='raw'):
+
+    temppath = tempfile.mkstemp(dir=str(cache_path), text=True)
+    context = Context()
+
+    with YamlCache.open(context, str(temppath)) as yc:
+        if from_cache == 'raw':
+            return _yaml.load(filename, shortname)
+        elif from_cache == 'cached':
+            _yaml.load(filename, shortname)
+
+            return _yaml.load(filename, shortname, yaml_cache=yc)
+        else:
+            assert False
 
 
 # Tests for list composition
