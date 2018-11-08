apache/buildstream

commit 6ccfab0b1b25990e406446d5cbe5aee83a5e158a

Fix cache corruption by scripts when layout and integration commands are used
Root directory was marked as a non-artifact mount, so not using
SafeHardLink.  However integration commands executed with write access
to the root directory.

Fixes #749

buildstream/scriptelement.py
@@ -201,16 +201,17 @@ class ScriptElement(Element):
         # Setup environment
         sandbox.set_environment(self.get_environment())
 
+        # Tell the sandbox to mount the install root
+        directories = {'/': False}
+
         # Mark the artifact directories in the layout
         for item in self.__layout:
-            if item['destination'] != '/':
-                if item['element']:
-                    sandbox.mark_directory(item['destination'], artifact=True)
-                else:
-                    sandbox.mark_directory(item['destination'])
+            destination = item['destination']
+            was_artifact = directories.get(destination, False)
+            directories[destination] = item['element'] or was_artifact
 
-        # Tell the sandbox to mount the install root
-        sandbox.mark_directory(self.__install_root)
+        for directory, artifact in directories.items():
+            sandbox.mark_directory(directory, artifact=artifact)
 
     def stage(self, sandbox):
 

tests/integration/script.py
@@ -155,3 +155,32 @@ def test_script_layout(cli, tmpdir, datafiles):
         text = f.read()
 
     assert text == "Hi\n"
+
+
+@pytest.mark.datafiles(DATA_DIR)
+def test_regression_cache_corruption(cli, tmpdir, datafiles):
+    project = str(datafiles)
+    checkout_original = os.path.join(cli.directory, 'checkout-original')
+    checkout_after = os.path.join(cli.directory, 'checkout-after')
+    element_name = 'script/corruption.bst'
+    canary_element_name = 'script/corruption-image.bst'
+
+    res = cli.run(project=project, args=['build', canary_element_name])
+    assert res.exit_code == 0
+
+    res = cli.run(project=project, args=['checkout', canary_element_name,
+                                         checkout_original])
+    assert res.exit_code == 0
+
+    with open(os.path.join(checkout_original, 'canary')) as f:
+        assert f.read() == 'alive\n'
+
+    res = cli.run(project=project, args=['build', element_name])
+    assert res.exit_code == 0
+
+    res = cli.run(project=project, args=['checkout', canary_element_name,
+                                         checkout_after])
+    assert res.exit_code == 0
+
+    with open(os.path.join(checkout_after, 'canary')) as f:
+        assert f.read() == 'alive\n'

tests/integration/project/elements/script/corruption-image.bst
@@ -1,0 +1,4 @@
+kind: import
+sources:
+- kind: local
+  path: files/canary

tests/integration/project/elements/script/corruption-integration.bst
@@ -1,0 +1,7 @@
+kind: stack
+
+public:
+  bst:
+    integration-commands:
+      - echo smashed >>/canary
+

tests/integration/project/elements/script/corruption.bst
@@ -1,0 +1,21 @@
+kind: script
+
+depends:
+- filename: base.bst
+  type: build
+- filename: script/corruption-image.bst
+  type: build
+- filename: script/corruption-integration.bst
+  type: build
+
+variables:
+  install-root: "/"
+
+config:
+  layout:
+  - element: base.bst
+    destination: "/"
+  - element: script/corruption-image.bst
+    destination: "/"
+  - element: script/corruption-integration.bst
+    destination: "/"

tests/integration/project/files/canary
@@ -1,0 +1,1 @@
+alive

commit cc2e6ae53cfaa1b476a17ee071bbf0ea2255079b

Fix bug with root mounted as non-artifact in script plugin.
The issue was introduced by 6ccfab0b1b25990e406446d5cbe5aee83a5e158a.

buildstream/scriptelement.py
@@ -202,7 +202,7 @@ class ScriptElement(Element):
         sandbox.set_environment(self.get_environment())
 
         # Tell the sandbox to mount the install root
-        directories = {'/': False}
+        directories = {self.__install_root: False}
 
         # Mark the artifact directories in the layout
         for item in self.__layout:
@@ -211,7 +211,10 @@ class ScriptElement(Element):
             directories[destination] = item['element'] or was_artifact
 
         for directory, artifact in directories.items():
-            sandbox.mark_directory(directory, artifact=artifact)
+            # Root does not need to be marked as it is always mounted
+            # with artifact (unless explicitly marked non-artifact)
+            if directory != '/':
+                sandbox.mark_directory(directory, artifact=artifact)
 
     def stage(self, sandbox):
 

tests/integration/project/elements/script/corruption-2.bst
@@ -1,0 +1,11 @@
+kind: script
+
+depends:
+- filename: base.bst
+  type: build
+- filename: script/corruption-image.bst
+  type: build
+
+config:
+  commands:
+  - echo smashed >>/canary

tests/integration/project/elements/script/marked-tmpdir.bst
@@ -1,0 +1,12 @@
+kind: compose
+
+depends:
+- filename: base.bst
+  type: build
+
+public:
+  bst:
+    split-rules:
+      remove:
+        - "/tmp/**"
+        - "/tmp"

tests/integration/project/elements/script/no-tmpdir.bst
@@ -1,0 +1,12 @@
+kind: filter
+
+depends:
+- filename: script/marked-tmpdir.bst
+  type: build
+
+config:
+  exclude:
+  - remove
+  include-orphans: True
+
+

tests/integration/project/elements/script/tmpdir.bst
@@ -1,0 +1,10 @@
+kind: script
+
+depends:
+- filename: script/no-tmpdir.bst
+  type: build
+
+config:
+  commands:
+  - |
+    mkdir -p /tmp/blah

tests/integration/script.py
@@ -184,3 +184,41 @@ def test_regression_cache_corruption(cli, tmpdir, datafiles):
 
     with open(os.path.join(checkout_after, 'canary')) as f:
         assert f.read() == 'alive\n'
+
+
+@pytest.mark.datafiles(DATA_DIR)
+def test_regression_tmpdir(cli, tmpdir, datafiles):
+    project = str(datafiles)
+    element_name = 'script/tmpdir.bst'
+
+    res = cli.run(project=project, args=['build', element_name])
+    assert res.exit_code == 0
+
+
+@pytest.mark.datafiles(DATA_DIR)
+def test_regression_cache_corruption_2(cli, tmpdir, datafiles):
+    project = str(datafiles)
+    checkout_original = os.path.join(cli.directory, 'checkout-original')
+    checkout_after = os.path.join(cli.directory, 'checkout-after')
+    element_name = 'script/corruption-2.bst'
+    canary_element_name = 'script/corruption-image.bst'
+
+    res = cli.run(project=project, args=['build', canary_element_name])
+    assert res.exit_code == 0
+
+    res = cli.run(project=project, args=['checkout', canary_element_name,
+                                         checkout_original])
+    assert res.exit_code == 0
+
+    with open(os.path.join(checkout_original, 'canary')) as f:
+        assert f.read() == 'alive\n'
+
+    res = cli.run(project=project, args=['build', element_name])
+    assert res.exit_code == 0
+
+    res = cli.run(project=project, args=['checkout', canary_element_name,
+                                         checkout_after])
+    assert res.exit_code == 0
+
+    with open(os.path.join(checkout_after, 'canary')) as f:
+        assert f.read() == 'alive\n'