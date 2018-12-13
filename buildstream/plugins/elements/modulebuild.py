6010b5a462963252a427b0922b156f472c32ad4e
Tristan Van Berkom <tristan.vanberkom@codethink.co.uk>
2018-12-13 18:43:03 +0900

    modulebuild plugin: Support virtual directories

--- buildstream/plugins/elements/modulebuild.py
+++ buildstream/plugins/elements/modulebuild.py
@@ -36,7 +36,8 @@ from buildstream import BuildElement
 
 # Element implementation for the 'modulebuild' kind.
 class ModuleBuildElement(BuildElement):
-    pass
+    # Supports virtual directories (required for remote execution)
+    BST_VIRTUAL_DIRECTORY = True
 
 
 # Plugin entry point
