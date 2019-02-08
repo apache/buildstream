4e1dbe5e879e16083be41165b5c3f9e57e47f320
Laurence Urhegyi <laurence.urhegyi@codethink.co.uk>
2019-02-08 16:04:44 +0000

    Update casremote.py

--- buildstream/_cas/casremote.py
+++ buildstream/_cas/casremote.py
@@ -14,6 +14,8 @@
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library. If not, see <http://www.gnu.org/licenses/>.
 #
+#  Authors:
+#        Raoul Hidaolgo Charman <raoulhidalgocharman@codethink.co.uk>
 
 from collections import namedtuple
 import io
