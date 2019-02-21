c7bf1f9636f3db3f6f77798f06233ffe8cced2eb
Martin Blanchard <martin.blanchard@codethink.co.uk>
2019-02-21 12:33:56 +0000

    Use 'instance-name' everywhere in remote execution config.
    Currently the 'artifacts' section expects 'instance_name', the
    'remote-execution' section expects 'instance' and documentation mentions
    'instance-name'... This patch changes this to use 'instance-name'
    everywhere.

--- buildstream/sandbox/_sandboxremote.py
+++ buildstream/sandbox/_sandboxremote.py
@@ -81,7 +81,7 @@ class SandboxRemote(Sandbox):
 
         if config.action_service:
             self.action_url = config.action_service['url']
-            self.action_instance = config.action_service.get('instance', None)
+            self.action_instance = config.action_service.get('instance-name', None)
             self.action_credentials = grpc.ssl_channel_credentials(
                 root_certificates=action_certs.get('server-cert'),
                 private_key=action_certs.get('client-key'),
@@ -91,8 +91,8 @@ class SandboxRemote(Sandbox):
             self.action_instance = None
             self.action_credentials = None
 
-        self.exec_instance = config.exec_service.get('instance', None)
-        self.storage_instance = config.storage_service.get('instance', None)
+        self.exec_instance = config.exec_service.get('instance-name', None)
+        self.storage_instance = config.storage_service.get('instance-name', None)
 
         self.storage_remote_spec = CASRemoteSpec(self.storage_url, push=True,
                                                  server_cert=config.storage_service['server-cert'],
@@ -133,10 +133,10 @@ class SandboxRemote(Sandbox):
         remote_exec_storage_config = require_node(remote_config, 'storage-service')
         remote_exec_action_config = remote_config.get('action-cache-service', {})
 
-        _yaml.node_validate(remote_exec_service_config, ['url', 'instance'] + tls_keys)
-        _yaml.node_validate(remote_exec_storage_config, ['url', 'instance'] + tls_keys)
+        _yaml.node_validate(remote_exec_service_config, ['url', 'instance-name'] + tls_keys)
+        _yaml.node_validate(remote_exec_storage_config, ['url', 'instance-name'] + tls_keys)
         if remote_exec_action_config:
-            _yaml.node_validate(remote_exec_action_config, ['url', 'instance'] + tls_keys)
+            _yaml.node_validate(remote_exec_action_config, ['url', 'instance-name'] + tls_keys)
         else:
             remote_config['action-service'] = None
 
