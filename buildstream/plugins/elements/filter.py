611bbae66b91569ff3cbc68bcb7acec076e46470
Jürg Billeter <j@bitron.ch>
2019-03-02 10:54:32 +0100

    filter.py: Set BST_VIRTUAL_DIRECTORY
    The filter plugin doesn't use Sandbox.get_directory().

--- buildstream/plugins/elements/filter.py
+++ buildstream/plugins/elements/filter.py
@@ -160,6 +160,9 @@ class FilterElement(Element):
     # added, to reduce the potential for confusion
     BST_FORBID_SOURCES = True
 
+    # This plugin has been modified to avoid the use of Sandbox.get_directory
+    BST_VIRTUAL_DIRECTORY = True
+
     def configure(self, node):
         self.node_validate(node, [
             'include', 'exclude', 'include-orphans'
