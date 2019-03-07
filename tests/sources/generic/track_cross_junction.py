7ecf392b990c1f9610f84df2ae3ed4b72afa5e64
Phil Dawson <phil.dawson@codethink.co.uk>
2019-03-07 14:47:50 +0000

    tests: move templated tests from track_cross_junction.py into separate file
    Move the templated tests in tests/frontend/track_cross_junction.py into
    sources/generic/track_cross_junction.py
    
    This is part of the preparation work for #944

--- tests/sources/generic/track_cross_junction.py
+++ tests/sources/generic/track_cross_junction.py
@@ -1,9 +1,32 @@
+#
+#  Copyright (C) 2018 Codethink Limited
+#  Copyright (C) 2019 Bloomberg Finance LP
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 2 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library. If not, see <http://www.gnu.org/licenses/>.
+#
+
 import os
 import pytest
+
 from tests.testutils import create_repo, ALL_REPO_KINDS, generate_junction
 
 from buildstream.plugintestutils import cli
 from buildstream import _yaml
+
+# Project directory
+TOP_DIR = os.path.dirname(os.path.realpath(__file__))
+DATA_DIR = os.path.join(TOP_DIR, 'project')
 
 
 def generate_element(repo, element_path, dep_name=None):
