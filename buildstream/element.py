20f3bbb978f79060cfc19f2b681f79018f9317a7
Jürg Billeter <j@bitron.ch>
2019-03-12 11:41:39 +0000

    element.py: Drop unneeded conditional for __weak_cached
    __weak_cached always needs to be recalculated when a new weak cache key
    is set. This doesn't change the behavior as __weak_cached is also set to
    None when __weak_cache_key is set to None.

--- buildstream/element.py
+++ buildstream/element.py
@@ -1199,8 +1199,7 @@ class Element(Plugin):
                 # Weak cache key could not be calculated yet
                 return
 
-            if not self.__weak_cached:
-                self.__weak_cached = self.__artifacts.contains(self, self.__weak_cache_key)
+            self.__weak_cached = self.__artifacts.contains(self, self.__weak_cache_key)
 
         if not context.get_strict():
             # Full cache query in non-strict mode requires both the weak and
