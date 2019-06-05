571e77bd59ec39a0393c69f7a53bd62d410e81ad
Benjamin Schubert <ben.c.schubert@gmail.com>
2019-06-05 14:53:01 +0100

    tests/testutils/site: Remove unused HAVE_OSTREE_CLI variable

--- tests/testutils/site.py
+++ tests/testutils/site.py
@@ -2,15 +2,6 @@
 # so we dont have to repeat this everywhere
 #
 
-from buildstream import utils, ProgramNotFoundError
-
-
-try:
-    OSTREE_CLI = utils.get_host_tool('ostree')
-    HAVE_OSTREE_CLI = True
-except ProgramNotFoundError:
-    HAVE_OSTREE_CLI = False
-
 try:
     from bst_plugins_experimental.sources import _ostree  # pylint: disable=unused-import
     HAVE_OSTREE = True
