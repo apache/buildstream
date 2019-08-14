2a47d489575136c8dd12e99c2266f3459ba77a52
Daniel Silverstone <daniel.silverstone@codethink.co.uk>
2019-08-14 08:08:35 +0100

    sandbox/_sandbox*.py: Ensure missing-command is a SandboxCommandError
    So that, should a sandbox be missing a command such as `sh`, we cache
    failures to build, ensure that each sandbox which checks for the command
    raises SandboxCommandError instead of SandboxError.
    
    Signed-off-by: Daniel Silverstone <daniel.silverstone@codethink.co.uk>

--- src/buildstream/sandbox/_sandboxdummy.py
+++ src/buildstream/sandbox/_sandboxdummy.py
@@ -17,7 +17,7 @@
 #  Authors:
 
 from .._exceptions import SandboxError
-from .sandbox import Sandbox
+from .sandbox import Sandbox, SandboxCommandError
 
 
 class SandboxDummy(Sandbox):
@@ -28,9 +28,9 @@ class SandboxDummy(Sandbox):
     def _run(self, command, flags, *, cwd, env):
 
         if not self._has_command(command[0], env):
-            raise SandboxError("Staged artifacts do not provide command "
-                               "'{}'".format(command[0]),
-                               reason='missing-command')
+            raise SandboxCommandError("Staged artifacts do not provide command "
+                                      "'{}'".format(command[0]),
+                                      reason='missing-command')
 
         raise SandboxError("This platform does not support local builds: {}".format(self._reason),
                            reason="unavailable-local-sandbox")
