3221f3511cbfe6a748b542627801acf8d0fcb379
Jürg Billeter <j@bitron.ch>
2019-09-02 09:47:05 +0200

    tests: Catch tests that don't shut down background threads

---
+++ src/buildstream/testing/_fixtures.py
@@ -0,0 +1,31 @@
+#
+#  Copyright (C) 2019 Bloomberg Finance LP
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 2 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library. If not, see <http://www.gnu.org/licenses/>.
+
+import psutil
+import pytest
+
+from buildstream import utils
+
+# Catch tests that don't shut down background threads, which could then lead
+# to other tests hanging when BuildStream uses fork().
+@pytest.fixture(autouse=True)
+def thread_check():
+    # xdist/execnet has its own helper thread.
+    # Ignore that for `utils._is_single_threaded` checks.
+    utils._INITIAL_NUM_THREADS_IN_MAIN_PROCESS = psutil.Process().num_threads()
+
+    yield
+    assert utils._is_single_threaded()
