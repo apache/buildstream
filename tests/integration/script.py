83c4a1fbe0737a5c16f4f071e29194789e0f366c
Jürg Billeter <j@bitron.ch>
2019-11-26 11:57:48 +0100

    tests/integration/script.py: xfail read-only-root for buildbox-run
    The root directory can't be marked read-only with buildbox-run.

--- tests/integration/script.py
+++ tests/integration/script.py
@@ -92,6 +92,9 @@ def test_script_root(cli, datafiles):
 @pytest.mark.datafiles(DATA_DIR)
 @pytest.mark.skipif(not HAVE_SANDBOX, reason="Only available with a functioning sandbox")
 @pytest.mark.xfail(HAVE_SANDBOX == "buildbox", reason="Not working with BuildBox")
+@pytest.mark.xfail(
+    HAVE_SANDBOX == "buildbox-run", reason="Read-only root directory not supported by buildbox-run",
+)
 def test_script_no_root(cli, datafiles):
     project = str(datafiles)
     element_path = os.path.join(project, "elements")
