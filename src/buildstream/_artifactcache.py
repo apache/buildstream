e08785a5004d6144a42e1672d79af9c4f823cda7
Darius Makovsky <traveltissues@protonmail.com>
2019-12-05 12:22:57 +0000

    _artifactcache: remove get_artifact_logs()

--- src/buildstream/_artifactcache.py
+++ src/buildstream/_artifactcache.py
@@ -26,7 +26,6 @@ from ._exceptions import ArtifactError, CASError, CacheError, CASRemoteError, Re
 from ._protos.buildstream.v2 import buildstream_pb2, buildstream_pb2_grpc, artifact_pb2, artifact_pb2_grpc
 
 from ._remote import BaseRemote
-from .storage._casbaseddirectory import CasBasedDirectory
 from ._artifact import Artifact
 from . import utils
 
@@ -412,21 +411,6 @@ class ArtifactCache(BaseCache):
 
         if not os.path.exists(os.path.join(self._basedir, newref)):
             os.link(os.path.join(self._basedir, oldref), os.path.join(self._basedir, newref))
-
-    # get_artifact_logs():
-    #
-    # Get the logs of an existing artifact
-    #
-    # Args:
-    #     ref (str): The ref of the artifact
-    #
-    # Returns:
-    #     logsdir (CasBasedDirectory): A CasBasedDirectory containing the artifact's logs
-    #
-    def get_artifact_logs(self, ref):
-        cache_id = self.cas.resolve_ref(ref, update_mtime=True)
-        vdir = CasBasedDirectory(self.cas, digest=cache_id).descend("logs")
-        return vdir
 
     # fetch_missing_blobs():
     #
