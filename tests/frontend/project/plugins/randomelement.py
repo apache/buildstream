f42205787e33174408012e5de3e3576acaab9f9b
Jürg Billeter <j@bitron.ch>
2020-04-19 10:34:24 +0200

    tests/frontend/push.py: Add test_push_after_rebuild

---
+++ tests/frontend/project/plugins/randomelement.py
@@ -0,0 +1,36 @@
+import os
+
+from buildstream import Element
+
+
+class RandomElement(Element):
+    BST_VIRTUAL_DIRECTORY = True
+
+    def configure(self, node):
+        pass
+
+    def preflight(self):
+        pass
+
+    def get_unique_key(self):
+        pass
+
+    def configure_sandbox(self, sandbox):
+        pass
+
+    def stage(self, sandbox):
+        pass
+
+    def assemble(self, sandbox):
+        rootdir = sandbox.get_virtual_directory()
+        outputdir = rootdir.descend("output", create=True)
+
+        # Generate non-reproducible output
+        with outputdir.open_file("random", mode="wb") as f:
+            f.write(os.urandom(64))
+
+        return "/output"
+
+
+def setup():
+    return RandomElement
