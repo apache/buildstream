0024c4688d1633612626e4513951d71d5bd02bc6
Jürg Billeter <j@bitron.ch>
2020-04-24 09:05:51 +0200

    tests/testutils/constants.py: Increase pexpect timeouts
    CI runners sometimes need more time under load.

--- tests/testutils/constants.py
+++ tests/testutils/constants.py
@@ -5,11 +5,11 @@
 #
 # Use this for operations that are expected to finish within a short amount of
 # time. Like `bst init`, `bst show` on a small project.
-PEXPECT_TIMEOUT_SHORT = 5
+PEXPECT_TIMEOUT_SHORT = 30
 
 
 # Timeout for longer interactive operations (in seconds).
 #
 # Use this for operations that are expected to take longer amounts of time,
 # like `bst build` on a small project.
-PEXPECT_TIMEOUT_LONG = 60
+PEXPECT_TIMEOUT_LONG = 300
