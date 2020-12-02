49a81f9244507225402bd50e2c974d0fe18dc084
Tristan van Berkom <tristan@codethink.co.uk>
2020-12-02 17:58:23 +0900

    _yaml.pyi: Adding declaration for _yaml.load()

--- src/buildstream/_yaml.pyi
+++ src/buildstream/_yaml.pyi
@@ -1,3 +1,5 @@
-# FIXME: This file is currently a placeholder so that mypy doesn't complain
-# about `_yaml` module being missing. In future, we should consider adding
-# stubs for Cythonized code.
+from typing import Optional
+
+from .node import MappingNode
+
+def load(filename: str, shortname: str, copy_tree: bool = False, project: Optional[object] = None) -> MappingNode: ...
