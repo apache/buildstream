8cfa27f632daf342d029aa7cc46f08e4a176cc9b
Tristan van Berkom <tristan@codethink.co.uk>
2021-01-22 17:41:32 +0900

    plugins/elements/junction.py: Remove ambiguous cache related junction parameters
    This removes the "cache-junction-elements" and "ignore-junction-remotes"
    parameters from the junction YAML configuration.
    
    As referenced by the mailing list thread[0], these parameters do not belong
    in element configuration, fine grained configuration of these parameters
    are not needed in the context of a pipeline with many subprojects in play,
    and the documentation and expected behavior with deep and complex subproject
    hierarches is unclear, especially when considering the ability to override
    junction configurations. Further, these currently cannot even be overridden
    by user configuration.
    
    This will be replaced by a simpler configuration in the regular way, with
    recommendations (not hard decisions) in project.conf and the final decision
    made by user configuration files.
    
    Summary of changes:
    
      * plugins/elements/junction.py: Removed the configuration and docs
    
      * _project.py: Stop observing these configurations
    
      * tests/artifactcache/junctions.py: Stop testing these configurations
    
    [0]: https://lists.apache.org/thread.html/rf2da9830e2fa918357f99a6021e55fc43df876f0b19d43f68802f083%40%3Cdev.buildstream.apache.org%3E

--- src/buildstream/_project.py
+++ src/buildstream/_project.py
@@ -867,16 +867,6 @@ class Project:
         # Load artifacts pull/push configuration for this project
         self.artifact_cache_specs = ArtifactCache.specs_from_config_node(config, self.directory)
 
-        # If there is a junction Element which specifies that we want to remotely cache
-        # its elements, append the junction's remotes to the artifact cache specs list
-        if self.junction:
-            parent = self.junction._get_project()
-            if self.junction.ignore_junction_remotes:
-                self.artifact_cache_specs = []
-
-            if self.junction.cache_junction_elements:
-                self.artifact_cache_specs = parent.artifact_cache_specs + self.artifact_cache_specs
-
         # Load source caches with pull/push config
         self.source_cache_specs = SourceCache.specs_from_config_node(config, self.directory)
 
