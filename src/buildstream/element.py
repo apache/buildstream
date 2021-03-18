apache/buildstream

commit 8d9bf0249cf5293402304aacb5af98ddd4b23c24

element.py: Stop computing cache keys when caching, we should have them
This removes a call to `__update_cache_key_non_strict()` in the job for
non strict builds. This call has an impact on other elements and should
never be done beforehand.

This however means we need to compute cache keys before, and we
therefore need to re-compute the cache keys as an element become
buildable

src/buildstream/element.py
@@ -1850,8 +1850,9 @@ class Element(Plugin):
             except VirtualDirectoryError:
                 pass
 
-        # ensure we have cache keys
-        self.__update_cache_key_non_strict()
+        # We should always have cache keys already set when caching an artifact
+        assert self.__cache_key is not None
+        assert self.__artifact._cache_key is not None
 
         with self.timed_activity("Caching artifact"):
             artifact_size = self.__artifact.cache(sandbox_build_dir, collectvdir, sourcesvdir, buildresult, publicdata)
@@ -2410,6 +2411,7 @@ class Element(Plugin):
                     assert not rdep.__build_deps_uncached < 0
 
                     if rdep.__buildable_callback is not None and rdep._buildable():
+                        rdep.__update_cache_key_non_strict()
                         rdep.__buildable_callback(rdep)
                         rdep.__buildable_callback = None
 

commit 7b5a125db3c7ccc5e81000bfef040fae8e6f9c24

element.py: Compute cache key also for rdep without buildable callback
The reverse dependency may not be in the build queue yet and thus, the
buildable callback may be `None`. However, it's still necessary to
update the cache key in non-strict mode.

Fixes #1452.

Fixes: 8d9bf024 ("element.py: Stop computing cache keys when...")

src/buildstream/element.py
@@ -2412,10 +2412,12 @@ class Element(Plugin):
                     rdep.__build_deps_uncached -= 1
                     assert not rdep.__build_deps_uncached < 0
 
-                    if rdep.__buildable_callback is not None and rdep._buildable():
+                    if rdep._buildable():
                         rdep.__update_cache_key_non_strict()
-                        rdep.__buildable_callback(rdep)
-                        rdep.__buildable_callback = None
+
+                        if rdep.__buildable_callback is not None:
+                            rdep.__buildable_callback(rdep)
+                            rdep.__buildable_callback = None
 
     # _walk_artifact_files()
     #