0038c135642fe331f8f42b65d8c8a02c1bebaabf
Tristan van Berkom <tristan@codethink.co.uk>
2021-03-21 14:27:58 +0900

    Updating all copyright headers to include the ASF license blurb instead of LGPL
    Using the sample text provided at the end of the Apache LICENSE file.
    
    Intentionally using the text in the LICENSE instead of the text recommended at:
    
        https://www.apache.org/legal/src-headers.html#header-text
    
    Because it is preferrable to preserve the original copyright notices in
    each file which still apply, even though there is a new copyright statement
    in the NOTICE file which applies to the "assembly" of the sources.

--- setup.py
+++ setup.py
@@ -2,18 +2,17 @@
 #
 #  Copyright (C) 2016 Codethink Limited
 #
-#  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU Lesser General Public
-#  License as published by the Free Software Foundation; either
-#  version 2 of the License, or (at your option) any later version.
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
 #
-#  This library is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
-#  Lesser General Public License for more details.
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
-#  You should have received a copy of the GNU Lesser General Public
-#  License along with this library. If not, see <http://www.gnu.org/licenses/>.
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
 #
 #  Authors:
 #        Tristan Van Berkom <tristan.vanberkom@codethink.co.uk>
@@ -320,7 +319,7 @@ setup(
     classifiers=[
         "Environment :: Console",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: POSIX",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
@@ -331,7 +330,7 @@ setup(
         "Topic :: Software Development :: Build Tools",
     ],
     description="A framework for modelling build pipelines in YAML",
-    license="LGPL",
+    license="Apache License Version 2.0",
     long_description=long_description,
     long_description_content_type="text/x-rst; charset=UTF-8",
     url="https://buildstream.build",
