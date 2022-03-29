863383d3660b38c8f725b06dc425cedd5716839c
Tristan van Berkom <tristan.vanberkom@codethink.co.uk>
2022-03-29 16:41:35 +0900

    tests/frontend/track.py: port tests to use internal git source
    In order to remove the `git` plugin, we're porting some of our test cases
    which we need to keep to use the private `git` plugin added to the test
    sample-plugins package.

--- tests/frontend/__init__.py
+++ tests/frontend/__init__.py
@@ -8,4 +8,12 @@ def configure_project(path, config):
     config["name"] = "test"
     config["min-version"] = "2.0"
     config["element-path"] = "elements"
+    config["plugins"] = [
+        {
+            "origin": "pip",
+            "package-name": "sample-plugins",
+            "sources": ["git"],
+        }
+    ]
+
     _yaml.roundtrip_dump(config, os.path.join(path, "project.conf"))
