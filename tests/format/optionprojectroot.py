57403e656bcd9ad826b096e99bd6b59bf4676e1e
Tristan van Berkom <tristan.vanberkom@codethink.co.uk>
2022-07-22 15:42:12 +0900

    tests/format/optionprojectroot.py: Test some restricted keys in the project.conf
    Test that we get the correct errors when attempting to conditionally specify
    keys which are not allowed to be specified conditionally.

--- tests/format/optionprojectroot.py
+++ tests/format/optionprojectroot.py
@@ -4,6 +4,7 @@
 import os
 import pytest
 from buildstream import _yaml
+from buildstream.exceptions import ErrorDomain, LoadErrorReason
 from buildstream._testing.runcli import cli  # pylint: disable=unused-import
 
 # Project directory
@@ -42,3 +43,26 @@ def test_resolve_element_override_conditional(cli, datafiles, value, expected):
     result.assert_success()
     loaded = _yaml.load_data(result.output)
     assert loaded.get_str("result") == expected
+
+
+#
+# Test that restricted keys error out correctly if specified conditionally
+#
+@pytest.mark.datafiles(DATA_DIR)
+@pytest.mark.parametrize(
+    "project_dir,provenance",
+    [
+        ("option-restricted-name", "project.conf [line 15 column 10]"),
+        ("option-restricted-options", "project.conf [line 16 column 6]"),
+    ],
+    ids=["name", "options"],
+)
+def test_restricted_conditionals(cli, datafiles, project_dir, provenance):
+    project = os.path.join(datafiles.dirname, datafiles.basename, project_dir)
+    result = cli.run(
+        project=project,
+        silent=True,
+        args=["show", "element.bst"],
+    )
+    result.assert_main_error(ErrorDomain.LOAD, LoadErrorReason.ILLEGAL_COMPOSITE)
+    assert provenance in result.stderr
