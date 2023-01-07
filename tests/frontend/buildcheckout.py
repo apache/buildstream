dc8b9c641d9b12bb78223d2f802f777ca5017fe5
TrellixVulnTeam <charles.mcfarland@trellix.com>
2023-01-07 02:45:33 +0000

    Adding tarfile member sanitization to extractall()

--- tests/frontend/buildcheckout.py
+++ tests/frontend/buildcheckout.py
@@ -609,7 +609,26 @@ def test_build_checkout_tarball_links(datafiles, cli):
     result.assert_success()
 
     with tarfile.open(name=checkout, mode="r:") as tar:
-        tar.extractall(extract)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner) 
+            
+        
+        safe_extract(tar, extract)
 
     with open(os.path.join(extract, "basicfolder", "basicsymlink"), encoding="utf-8") as fp:
         data = fp.read()
