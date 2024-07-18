2daa04a0f3256ca8f444cba42d00b856d5a85cc6
Jürg Billeter <j@bitron.ch>
2024-07-18 20:29:15 +0200

    ci: Run tests also on Ubuntu 20.04
    Ubuntu 20.04 uses Python 3.8, which is not covered by any other CI
    images.

--- .github/run-ci.sh
+++ .github/run-ci.sh
@@ -102,7 +102,7 @@ function runServiceTest() {
 
 
 if [ -z "${test_names}" ]; then
-    for test_name in mypy debian-10 fedora-38 fedora-39 fedora-missing-deps; do
+    for test_name in mypy debian-10 fedora-38 fedora-39 fedora-missing-deps ubuntu-20.04; do
 	if ! runTest "${test_name}"; then
 	    echo "Tests failed"
 	    exit 1
