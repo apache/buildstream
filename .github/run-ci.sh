6f051005ef62d9744af6cb7f9262f2b6fd2095d3
Jürg Billeter <j@bitron.ch>
2024-11-24 17:00:08 +0100

    ci: Run tests also on Fedora 41
    This extends test coverage to Python 3.13.

--- .github/run-ci.sh
+++ .github/run-ci.sh
@@ -102,7 +102,7 @@ function runServiceTest() {
 
 
 if [ -z "${test_names}" ]; then
-    for test_name in mypy debian-11 fedora-40 fedora-missing-deps ubuntu-22.04; do
+    for test_name in mypy debian-11 fedora-40 fedora-41 fedora-missing-deps ubuntu-22.04; do
 	if ! runTest "${test_name}"; then
 	    echo "Tests failed"
 	    exit 1
