cb73060054eb01ebaf924b890969aee671541820
Jürg Billeter <j@bitron.ch>
2025-07-04 09:25:56 +0200

    tests/integration/sandbox.py: Add test for remote action cache config

--- tests/integration/sandbox.py
+++ tests/integration/sandbox.py
@@ -21,6 +21,7 @@ import pytest
 from buildstream._testing import cli_integration as cli  # pylint: disable=unused-import
 from buildstream._testing._utils.site import HAVE_SANDBOX
 
+from tests.testutils import create_artifact_share
 
 pytestmark = pytest.mark.integration
 
@@ -59,3 +60,24 @@ def test_remote_apis_socket(cli, datafiles):
 
     result = cli.run(project=project, args=["build", element_name])
     assert result.exit_code == 0
+
+
+# Test configuration with remote action cache for nested REAPI.
+@pytest.mark.skipif(not HAVE_SANDBOX, reason="Only available with a functioning sandbox")
+@pytest.mark.datafiles(DATA_DIR)
+def test_remote_apis_socket_with_action_cache(cli, tmpdir, datafiles):
+    project = str(datafiles)
+    element_name = "sandbox/remote-apis-socket.bst"
+
+    with create_artifact_share(os.path.join(str(tmpdir), "remote")) as share:
+        cli.configure(
+            {
+                "remote-execution": {
+                    "storage-service": {"url": share.repo},
+                    "action-cache-service": {"url": share.repo, "push": True},
+                }
+            }
+        )
+
+        result = cli.run(project=project, args=["build", element_name])
+        assert result.exit_code == 0
