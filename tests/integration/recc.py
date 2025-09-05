dbc5cc60140816528f33fc5fb2faf6969d8c5d35
Jürg Billeter <j@bitron.ch>
2025-09-05 10:41:04 +0200

    tests/integration/recc.py: Add recc cache-only test

--- tests/integration/recc.py
+++ tests/integration/recc.py
@@ -140,3 +140,116 @@ def test_remote_execution(cli, datafiles):
 
     # Verify recc is getting action cache hits for both, compiling and linking
     assert recc_log.count("Action Cache hit") == 2
+
+
+@pytest.mark.skipif(not HAVE_SANDBOX, reason="Only available with a functioning sandbox")
+@pytest.mark.datafiles(DATA_DIR)
+def test_cache_only(cli, datafiles):
+    project = str(datafiles)
+    checkout1 = os.path.join(cli.directory, "checkout1")
+    checkout2 = os.path.join(cli.directory, "checkout2")
+    element_name = "recc/cacheonly.bst"
+
+    # Always cache buildtrees to be able to check recc logs
+    result = cli.run(project=project, args=["--cache-buildtrees", "always", "build", element_name])
+    if result.exit_code != 0:
+        # Output recc logs in case of failure
+        cli.run(
+            project=project,
+            args=[
+                "shell",
+                "--build",
+                "--use-buildtree",
+                element_name,
+                "--",
+                "sh",
+                "-c",
+                "cat config.log .recc-log/* */.recc-log/*",
+            ],
+        )
+    assert result.exit_code == 0
+
+    result = cli.run(project=project, args=["artifact", "checkout", element_name, "--directory", checkout1])
+    assert result.exit_code == 0
+
+    assert_contains(
+        checkout1,
+        [
+            "/usr",
+            "/usr/bin",
+            "/usr/share",
+            "/usr/bin/hello",
+            "/usr/share/doc",
+            "/usr/share/doc/amhello",
+            "/usr/share/doc/amhello/README",
+        ],
+    )
+
+    # Check the main build log
+    result = cli.run(project=project, args=["artifact", "log", element_name])
+    assert result.exit_code == 0
+    log = result.output
+
+    # Verify we get expected output exactly once
+    assert log.count("Making all in src") == 1
+
+    result = cli.run(
+        project=project,
+        args=[
+            "shell",
+            "--build",
+            "--use-buildtree",
+            element_name,
+            "--",
+            "sh",
+            "-c",
+            "cat src/.recc-log/recc.buildbox*",
+        ],
+    )
+    assert result.exit_code == 0
+    recc_log = result.output
+
+    # Verify recc is using local execution for both, compiling and linking
+    assert recc_log.count("Action not cached and running in cache-only mode, executing locally") == 2
+
+    # Delete artifact from BuildStream cache to trigger a BuildStream rebuild with action cache hits for recc
+    result = cli.run(project=project, args=["artifact", "delete", element_name])
+    assert result.exit_code == 0
+
+    result = cli.run(project=project, args=["--cache-buildtrees", "always", "build", element_name])
+    assert result.exit_code == 0
+
+    result = cli.run(project=project, args=["artifact", "checkout", element_name, "--directory", checkout2])
+    assert result.exit_code == 0
+
+    assert_contains(
+        checkout2,
+        [
+            "/usr",
+            "/usr/bin",
+            "/usr/share",
+            "/usr/bin/hello",
+            "/usr/share/doc",
+            "/usr/share/doc/amhello",
+            "/usr/share/doc/amhello/README",
+        ],
+    )
+
+    result = cli.run(
+        project=project,
+        args=[
+            "shell",
+            "--build",
+            "--use-buildtree",
+            element_name,
+            "--",
+            "sh",
+            "-c",
+            "cat src/.recc-log/recc.buildbox*",
+        ],
+    )
+    assert result.exit_code == 0
+    recc_log = result.output
+
+    # Verify recc is getting action cache hits for both, compiling and linking
+    assert recc_log.count("Action Cache hit") == 2
