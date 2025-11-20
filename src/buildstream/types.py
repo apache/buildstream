792164911fcf8090bf7afdd947c252953c5f56b7
Joshua Zivkovic <joshua.zivkovic@codethink.co.uk>
2025-11-20 12:57:06 +0000

    Add more source provenance attributes
    Expand the selection of source provennce attributes according to #2069
    
    This adds:
    - attribution-text
    - concluded-license
    - copyright-text
    - declared-license
    - description
    - name
    - supplier

--- src/buildstream/types.py
+++ src/buildstream/types.py
@@ -395,13 +395,41 @@ class _SourceMirror:
 # A simple object describing user provided source provenance information
 #
 # Args:
-#    homepage: The project homepage URL
-#    issue_tracker: The project issue reporting URL
+#    attribution_text: Required acknowledgements for the source
+#    concluded_license: The license of the source as declared by the authors
+#    copyright_text: Copyright notice of the source
+#    declared_license: Licences that have been officially declared for the source
+#    description: Description of the source
+#    external_reference: Reference to external inforamtion or assets relevant to the source
+#    homepage: The source's homepage URL
+#    issue_tracker: The source's issue reporting URL
+#    name: Name of the source
+#    originator: The name of the source's originators/authors
 #
 class _SourceProvenance:
-    def __init__(self, homepage: Optional[str], issue_tracker: Optional[str]):
+    def __init__(
+        self,
+        attribution_text: Optional[str],
+        concluded_license: Optional[str],
+        copyright_text: Optional[str],
+        declared_license: Optional[str],
+        description: Optional[str],
+        external_reference: Optional[str],
+        homepage: Optional[str],
+        issue_tracker: Optional[str],
+        name: Optional[str],
+        originator: Optional[str],
+    ):
+        self.attribution_text: Optional[str] = attribution_text
+        self.concluded_license: Optional[str] = concluded_license
+        self.copyright_text: Optional[str] = copyright_text
+        self.declared_license: Optional[str] = declared_license
+        self.description: Optional[str] = description
+        self.external_reference: Optional[str] = external_reference
         self.homepage: Optional[str] = homepage
         self.issue_tracker: Optional[str] = issue_tracker
+        self.name: Optional[str] = name
+        self.originator: Optional[str] = originator
 
     # new_from_node():
     #
@@ -418,12 +446,44 @@ class _SourceProvenance:
     #
     @classmethod
     def new_from_node(cls, node: MappingNode) -> "_SourceProvenance":
-        node.validate_keys(["homepage", "issue-tracker"])
+        node.validate_keys(
+            [
+                "attribution-text",
+                "concluded-license",
+                "copyright-text",
+                "declared-license",
+                "external-reference",
+                "description",
+                "homepage",
+                "issue-tracker",
+                "name",
+                "originator",
+            ]
+        )
 
+        attribution_text: Optional[str] = node.get_str("attribution-text", None)
+        concluded_license: Optional[str] = node.get_str("concluded-license", None)
+        copyright_text: Optional[str] = node.get_str("copyright-text", None)
+        declared_license: Optional[str] = node.get_str("declared-license", None)
+        description: Optional[str] = node.get_str("description", None)
+        external_reference: Optional[str] = node.get_str("external-reference", None)
         homepage: Optional[str] = node.get_str("homepage", None)
         issue_tracker: Optional[str] = node.get_str("issue-tracker", None)
+        name: Optional[str] = node.get_str("name", None)
+        originator: Optional[str] = node.get_str("originator", None)
 
-        return cls(homepage, issue_tracker)
+        return cls(
+            attribution_text,
+            concluded_license,
+            copyright_text,
+            declared_license,
+            description,
+            external_reference,
+            homepage,
+            issue_tracker,
+            name,
+            originator,
+        )
 
 
 ########################################
