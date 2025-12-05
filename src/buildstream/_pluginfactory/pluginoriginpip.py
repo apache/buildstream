54508d307a9f066d4c07e4681fcfe6a006718d87
kotborealis <kotborealis@awooo.ru>
2025-12-05 20:09:53 +0300

    Remove unused import of 'os' module
    Removed unused import of 'os' module.

--- src/buildstream/_pluginfactory/pluginoriginpip.py
+++ src/buildstream/_pluginfactory/pluginoriginpip.py
@@ -11,7 +11,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
-import os
 from importlib.util import find_spec
 from pathlib import Path
 
